e0d96108d6983556129b6161d4f2049dfbd358d6
Darin Morrison <dmorri23@cs.mcgill.ca>
2008-10-15 16:16:03 -0400

    slight reorganization/cleanup of parser interface

--- src/frontend/parser.mli
+++ src/frontend/parser.mli
@@ -10,8 +10,28 @@ open Core.Syntax
 
 
 
-val parse_stream : ?name:string -> input:char Stream.t -> Ext.sgn_decl list
+module Grammar : Camlp4.Sig.Grammar.Static
+  with module Loc   = Lexer.Loc
+  and  module Token = Lexer.Token
 
-val parse_string : ?name:string -> input:string        -> Ext.sgn_decl list
 
-val parse_file   :  name:string ->                        Ext.sgn_decl list
+
+(** Parse a stream and return a signature *)
+val parse_stream :
+     ?name:string
+  -> input:char Stream.t
+  -> 'a Grammar.Entry.t
+  -> 'a
+
+(** Parse a string and return a signature *)
+val parse_string :
+     ?name:string
+  -> input:string
+  -> 'a Grammar.Entry.t
+  -> 'a
+
+(** Parse a file and return a signature *)
+val parse_file   :
+     file_name:string
+  -> 'a Grammar.Entry.t
+  -> 'a

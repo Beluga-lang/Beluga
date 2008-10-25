834e6f9ac4b4d2658ee5c4c763a77cc235d7f33c
Darin Morrison <dmorri23@cs.mcgill.ca>
2008-10-24 23:43:27 -0400

    open Syntax.Int as with other modules -- Internal syntax clear from context

--- src/core/typecheck.ml
+++ src/core/typecheck.ml
@@ -8,9 +8,8 @@
 
 exception Error of string
 
-module I = Syntax.Int
-
 open Store.Cid
+open Syntax.Int
 
     (* check cD cPsi (tM, s1) (tA, s2) = ()
 
@@ -25,13 +24,13 @@ open Store.Cid
        otherwise exception Error is raised
     *)
     let rec checkW cD cPsi sM1 sA2 = match (sM1, sA2) with
-      | ((I.Lam(_,tM), s1), (I.PiTyp((I.TypDecl(x,tA) as tX), tB), s2)) ->
-          check cD  (I.DDec(cPsi, I.decSub tX s2))
-                 (tM, I.dot1 s1) (tB, I.dot1 s2)
-      | ((I.Root (h, tS), s), (((I.Atom _),s') as sP)) ->
+      | ((Lam(_,tM), s1), (PiTyp((TypDecl(x,tA) as tX), tB), s2)) ->
+          check cD  (DDec(cPsi, decSub tX s2))
+                 (tM, dot1 s1) (tB, dot1 s2)
+      | ((Root (h, tS), s), (((Atom _),s') as sP)) ->
         let
           (* cD ; cPsi |- [s]tA <= type  where sA = [s]tA *)
-          sA = Whnf.whnfTyp (inferHead cD cPsi h, I.id)
+          sA = Whnf.whnfTyp (inferHead cD cPsi h, id)
         in
           checkSpine cD cPsi (tS, s) sA sP
 
@@ -48,25 +47,25 @@ open Store.Cid
             and  cD ; cPsi |- tA'[s'] = tA [s2] <= type
             and  cD ; cPsi |- tP'[s'] = sP     <= type
     *)
-    and checkSpine cD cPsi sS1 sA2 (sP : I.tclo) = match (sS1, sA2) with
-        ((I.Nil, _), sP') ->
+    and checkSpine cD cPsi sS1 sA2 (sP : tclo) = match (sS1, sA2) with
+        ((Nil, _), sP') ->
             if Whnf.convTyp sP' sP then ()
             else raise (Error "Type mismatch")
 
-      | ((I.SClo (tS, s'), s), sA) ->
-          checkSpine cD cPsi (tS, I.comp s' s) sA sP
+      | ((SClo (tS, s'), s), sA) ->
+          checkSpine cD cPsi (tS, comp s' s) sA sP
 
-      | ((I.App (tM, tS), s1), (I.PiTyp (I.TypDecl (_, tA1), tB2), s2)) ->
+      | ((App (tM, tS), s1), (PiTyp (TypDecl (_, tA1), tB2), s2)) ->
          (check cD cPsi (tM, s1) (tA1, s2);
            let
              (* cD ; Psi1 |- tM <= tA1',  cD ; cPsi |- s1 <= Psi1
                 cD ; Psi2, x:tA1 |- tB2 <= type  and [s1]tA1' = [s2]tA1 *)
-             tB2 = Whnf.whnfTyp (tB2, I.Dot (I.Obj (I.Clo(tM,s1)), s2))
+             tB2 = Whnf.whnfTyp (tB2, Dot (Obj (Clo(tM,s1)), s2))
            in 
              checkSpine cD cPsi (tS, s1) tB2 sP
           )
 
-      | ((I.App (tM , tS), _),  (tA, s)) -> (* tA <> (Pi x:tA1. tA2, s) *)
+      | ((App (tM , tS), _),  (tA, s)) -> (* tA <> (Pi x:tA1. tA2, s) *)
           raise  (Error "Expression is applied, but not a function")
 
     (* inferHead cD cPsi h = tA
@@ -77,47 +76,47 @@ open Store.Cid
        else exception Error is raised. 
     *)
     and inferHead cD cPsi head = match head with
-         I.BVar k' ->
-           let I.TypDecl (_, tA) = I.ctxDec cPsi k'
+         BVar k' ->
+           let TypDecl (_, tA) = ctxDec cPsi k'
            in tA
-      | I.Proj (I.BVar(k'),  target) ->
-        let I.SigmaDecl(_, recA) = I.ctxSigmaDec cPsi k'
+      | Proj (BVar(k'),  target) ->
+        let SigmaDecl(_, recA) = ctxSigmaDec cPsi k'
           (* getType traverses the type from left to right;
              target is relative to the remaining suffix of the type
           *)
         in let rec getType s_recA target j = match (s_recA, target) with
-            | ((tA::recA, s), 1) -> I.TClo(tA, s)
+            | ((tA::recA, s), 1) -> TClo(tA, s)
             | ((tA::recA, s), target) ->
-                    let tPj = I.Root(I.Proj(I.BVar k', j), I.Nil) 
+                    let tPj = Root(Proj(BVar k', j), Nil) 
                     in 
-                      getType (recA, I.Dot(I.Obj(tPj), s)) (target-1) (j+1)
+                      getType (recA, Dot(Obj(tPj), s)) (target-1) (j+1)
         in 
-          getType (recA, I.id) target 1
+          getType (recA, id) target 1
    
       (* Missing cases?  Tue Sep 30 22:09:27 2008 -bp 
 
-         Proj (I.PVar(p,s), i) 
-         Proj (I.MVar(p,s), i) 
+         Proj (PVar(p,s), i) 
+         Proj (MVar(p,s), i) 
 
          These cases are impossible at the moment.
        *)
-      | I.Const c -> (Term.get c).Term.typ
+      | Const c -> (Term.get c).Term.typ
 
-      | I.MVar(I.Offset u, s) ->
+      | MVar(Offset u, s) ->
         let
           (* cD ; cPsi' |- tA <= type *)
-          (tA, cPsi') = I.mctxMDec cD u
+          (tA, cPsi') = mctxMDec cD u
         in 
           (checkSub cD cPsi s cPsi';
-           I.TClo(tA, s))
+           TClo(tA, s))
 
-      | I.PVar(I.Offset u,s) ->
+      | PVar(Offset u,s) ->
         let
           (* cD ; cPsi' |- tA <= type *)
-          (tA, cPsi') = I.mctxPDec cD u
+          (tA, cPsi') = mctxPDec cD u
         in 
           (   checkSub cD cPsi s cPsi';
-              I.TClo(tA, s)   )
+              TClo(tA, s)   )
 
     (* checkSub cD cPsi s cPsi' = ()
 
@@ -125,48 +124,48 @@ open Store.Cid
        This function succeeds iff cD ; cPsi |- s : cPsi'
     *)
     and checkSub cD cPsi s cPsi' = match (cPsi, s, cPsi') with
-         (I.Null, I.Shift 0, I.Null) -> ()
-      | (I.CtxVar(psi), I.Shift 0, I.CtxVar(psi')) ->
+         (Null, Shift 0, Null) -> ()
+      | (CtxVar(psi), Shift 0, CtxVar(psi')) ->
            if psi = psi' then () else raise (Error "ctx variable mismatch")
       (* SVar case to be added - bp *)
-      | (I.DDec (cPsi, tX), I.Shift k, I.Null) ->
-           if k>0 then checkSub cD cPsi (I.Shift (k-1)) I.Null
+      | (DDec (cPsi, tX), Shift k, Null) ->
+           if k>0 then checkSub cD cPsi (Shift (k-1)) Null
            else raise (Error "Substitution not well-typed")
 
-      | (cPsi', I.Shift k, cPsi) ->
-          checkSub cD cPsi' (I.Dot (I.Head(I.BVar (k+1)), I.Shift (k+1))) cPsi
+      | (cPsi', Shift k, cPsi) ->
+          checkSub cD cPsi' (Dot (Head(BVar (k+1)), Shift (k+1))) cPsi
 
-      | (cPsi', I.Dot (I.Head h, s'), 
-                           I.DDec (cPsi, (I.TypDecl (_, tA2)))) ->
+      | (cPsi', Dot (Head h, s'), 
+                           DDec (cPsi, (TypDecl (_, tA2)))) ->
         (* changed order of subgoals here Sun Dec  2 12:14:27 2001 -fp *)
         let 
            _ = checkSub cD cPsi' s' cPsi
           (* ensures that s' is well-typed before comparing types tA1 =[s']tA2 *)
           and tA1 = inferHead cD cPsi' h
         in
-          if Whnf.convTyp (tA1, I.id) (tA2, s') then ()
+          if Whnf.convTyp (tA1, id) (tA2, s') then ()
           else raise (Error ("Substitution not well-typed \n  found: " 
                             (* Print.expToString (cD, cPsi', tA1) ^ "\n  expected: " ^
-                            Print.expToString (cD, cPsi', I.Clo (tA2, s'))*)))
+                            Print.expToString (cD, cPsi', Clo (tA2, s'))*)))
 
-      | (cPsi', I.Dot (I.Head(I.BVar w), t), 
-                           I.SigmaDec (cPsi, (I.SigmaDecl (_, arec)))) ->
+      | (cPsi', Dot (Head(BVar w), t), 
+                           SigmaDec (cPsi, (SigmaDecl (_, arec)))) ->
         (* other heads of type Sigma disallowed -bp *)
         let
           _ = checkSub cD cPsi' t cPsi
           (* ensures that t is well-typed before comparing types BRec =[t]ARec *)
-        and I.SigmaDecl (_, brec) = I.ctxSigmaDec cPsi' w
+        and SigmaDecl (_, brec) = ctxSigmaDec cPsi' w
         in
-          if Whnf.convTypRec (brec, I.id) (arec, t)  then ()
+          if Whnf.convTypRec (brec, id) (arec, t)  then ()
           else raise (Error "Declaration not well-typed")
 
-      | (cPsi', I.Dot (I.Obj tM, s'), I.DDec (cPsi, (I.TypDecl (_, tA2)))) ->
+      | (cPsi', Dot (Obj tM, s'), DDec (cPsi, (TypDecl (_, tA2)))) ->
         (* changed order of subgoals here Sun Dec  2 12:15:53 2001 -fp *)
         let 
           _ = checkSub cD cPsi' s' cPsi
           (* ensures that s' is well-typed and [s']tA2 is well-defined *)
         in
-          check cD cPsi' (tM, I.id) (tA2, s')
+          check cD cPsi' (tM, id) (tA2, s')
 
 
     (* ---------------------------------------------------------- *)
@@ -181,23 +180,23 @@ open Store.Cid
        iff  cD ; cPsi |- [s1]tS <= [s2]K
      *)
     and checkSpineK cD cPsi sS1 sA = match (sS1, sA) with
-      | ((I.Nil, _), (I.Typ,s)) -> ()
-      | ((I.Nil, _), _) ->  raise (Error "kind mismatch")
-      | ((I.SClo (tS, s'), s), sA) ->
-          checkSpineK cD cPsi (tS, I.comp s' s) sA
-      | ((I.App (tM, tS), s1), (I.PiKind (I.TypDecl (_, tA1), kind), s2)) ->
+      | ((Nil, _), (Typ,s)) -> ()
+      | ((Nil, _), _) ->  raise (Error "kind mismatch")
+      | ((SClo (tS, s'), s), sA) ->
+          checkSpineK cD cPsi (tS, comp s' s) sA
+      | ((App (tM, tS), s1), (PiKind (TypDecl (_, tA1), kind), s2)) ->
           (   check cD cPsi (tM, s1) (tA1, s2);
-              checkSpineK cD cPsi (tS, s1) (kind, I.Dot (I.Obj (I.Clo (tM, s1)), s2))   )
-      | ((I.App (tM , tS), _), (tA, s)) ->
+              checkSpineK cD cPsi (tS, s1) (kind, Dot (Obj (Clo (tM, s1)), s2))   )
+      | ((App (tM , tS), _), (tA, s)) ->
           raise  (Error "Expression is applied, but not a function")
 
     (* checkTyp (cD, cPsi, (tA,s)) succeeds iff cD ; cPsi |- [s]tA <= type *)
     let rec checkTyp' (cD, cPsi, (tA,s)) = match (tA, s) with
-         (I.Atom(a, tS),s) ->
-            checkSpineK cD cPsi (tS,s) ((Typ.get a).Typ.kind, I.id)
-      | (I.PiTyp(I.TypDecl(x, tA), tB), s)  ->
+         (Atom(a, tS),s) ->
+            checkSpineK cD cPsi (tS,s) ((Typ.get a).Typ.kind, id)
+      | (PiTyp(TypDecl(x, tA), tB), s)  ->
         (   checkTyp cD cPsi (tA,s); 
-            checkTyp cD (I.DDec(cPsi, I.TypDecl(x, I.TClo(tA,s)))) (tB, I.dot1 s)   )
+            checkTyp cD (DDec(cPsi, TypDecl(x, TClo(tA,s)))) (tB, dot1 s)   )
 
     and checkTyp cD cPsi sA = checkTyp' (cD, cPsi, Whnf.whnfTyp sA)
 
@@ -206,33 +205,33 @@ open Store.Cid
          [] -> ()
       | tA::recA ->
         (checkTyp cD cPsi (tA, s);
-         checkTypRec cD (I.DDec(cPsi, I.decSub (I.TypDecl(Id.mk_name None, tA)) s))
-                     (recA, I.dot1 s))
+         checkTypRec cD (DDec(cPsi, decSub (TypDecl(Id.mk_name None, tA)) s))
+                     (recA, dot1 s))
 
     (* checkKind cD cPsi K succeeds iff cD ; cPsi |- K kind *)
     let rec checkKind cD cPsi kind = match kind with
-          I.Typ -> ()
-      | I.PiKind(I.TypDecl(x, tA), kind) ->
-         (checkTyp cD cPsi (tA, I.id); 
-          checkKind cD (I.DDec(cPsi, I.TypDecl(x, tA))) kind)
+          Typ -> ()
+      | PiKind(TypDecl(x, tA), kind) ->
+         (checkTyp cD cPsi (tA, id); 
+          checkKind cD (DDec(cPsi, TypDecl(x, tA))) kind)
 
 
     (* checkDec cD cPsi (x:tA, s) succeeds iff cD ; cPsi |- [s]tA <= type *)
     and checkDec cD cPsi (decl, s) = match decl with
-        I.TypDecl (_, tA) -> checkTyp cD cPsi (tA, s)
+        TypDecl (_, tA) -> checkTyp cD cPsi (tA, s)
 
     (* checkSigmaDec cD cPsi (x:recA, s) succeeds iff cD ; cPsi |- [s]recA <= type *)
     and checkSigmaDec cD cPsi (sigma_decl, s) = match sigma_decl with
-        I.SigmaDecl (_, arec) ->
+        SigmaDecl (_, arec) ->
             checkTypRec cD cPsi (arec, s)
 
     (* checkCtx (cD, cPsi) succeeds iff cD |- cPsi ctx *)
-    and checkCtx cD cPsi = match cPsi with I.Null ->  ()
-      | I.DDec (cPsi, tX) ->
-          (checkCtx cD cPsi; checkDec cD cPsi (tX, I.id))
-      | I.SigmaDec (cPsi, tX) ->
-          (checkCtx cD cPsi; checkSigmaDec cD cPsi (tX, I.id))
-      | I.CtxVar psi -> () 
+    and checkCtx cD cPsi = match cPsi with Null ->  ()
+      | DDec (cPsi, tX) ->
+          (checkCtx cD cPsi; checkDec cD cPsi (tX, id))
+      | SigmaDec (cPsi, tX) ->
+          (checkCtx cD cPsi; checkSigmaDec cD cPsi (tX, id))
+      | CtxVar psi -> () 
         (* need to check if psi is in Omega (or cD, if context vars live there) -bp *) 
 
     let rec check_sgn_decls decls = ()

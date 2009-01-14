b2a438b1f05dee19c7d335f9538a3817205a3d75
Renaud Germain <renaud.germain@mail.mcgill.ca>
2009-01-14 13:53:32 -0500

    took error type(s) out of Whnf/Check/Reconstruct to avoid cycles

---
+++ src/lib/core/error.ml
@@ -0,0 +1,20 @@
+(* -*- coding: utf-8; indent-tabs-mode: nil; -*- *)
+
+open Syntax
+
+module Error = struct
+
+  type error =
+    | CtxVarMisMatch of Int.LF.cvar * Int.LF.cvar
+    | SigmaIllTyped of Int.LF.mctx * Int.LF.dctx
+        * Int.LF.trec_clo (* inferred *) * Int.LF.trec_clo (* expected *)
+    | ExpAppNotFun  
+    | ExpNilNotAtom
+    | KindMisMatch 
+    | SubIllTyped      
+    | TypIllTyped of Int.LF.mctx * Int.LF.dctx
+        * Int.LF.tclo (* inferred *) * Int.LF.tclo (* expected *) 
+    | TypMisMatch of Int.LF.mctx * Int.LF.dctx * Int.LF.tclo * Int.LF.tclo
+    | IllTyped of Int.LF.mctx * Int.LF.dctx * Int.LF.nclo * Int.LF.tclo
+
+end

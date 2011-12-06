e7e3b382221ff1b56072de324430d5b7f087bf07
Mathieu Boespflug <mboes@cs.mcgill.ca>
2011-12-05 23:37:43 -0500

    Use only named capture groups.
    Comment avoiding substitution requires it, and it's good documentation anyways.

--- tools/syntax-conv.rb
+++ tools/syntax-conv.rb
@@ -29,11 +29,11 @@ class CompBlock < Block
     content.gsub!(/(?<leadin>(::|->|:|\})\s*)(?<obj>[^\{\}]*?)\s*\[\s*(?<ctx>.*?)\s*\]/m, '\k<leadin> [\k<ctx>. \k<obj>]')
     content.gsub! /\(\s*\[\s*(?<ctx>[^\.]*?)\s*\]\s*(?<obj>((?<pobj>\(([^()]+|\g<pobj>)*\))|[^()])+)\s*\)/m,
                   '[\k<ctx>. \k<obj>]'
-    content.gsub! /(=\s*)\[\s*([^\.\[\]%]*)\s*\]\s*([^\[\]]*?)(\s*in)/m, '\1[\2. \3]\4'
-    content.gsub! /(case\s*)\[\s*([^\.\[\]%]*)\s*\]\s*([^\[\]]*?)(\s*of)/m, '\1[\2. \3]\4'
-    content.gsub! /\[\s*([^\.\[\]%]*)\s*\]\s*([^\[\]]*?)(\s*)(;|=>|=|\|)/m, '[\1. \2]\3\4'
-    content.gsub! /<(.*?)>/m, '[\1]'
-    content.gsub! /\{(\s*.*?:)\((.*?)\)\*(\s*)\}/m, '{\1\2\3}'
+    content.gsub! /(?<leadin>=\s*)\[\s*(?<ctx>[^\.\[\]%]*)\s*\]\s*(?<obj>[^\[\]]*?)(?<leadout>\s*in)/m, '\k<leadin>[\<ctx>. \k<obj>]\k<leadout>'
+    content.gsub! /(?<leadin>case\s*)\[\s*(?<ctx>[^\.\[\]%]*)\s*\]\s*(?<obj>[^\[\]]*?)(?<leadout>\s*of)/m, '\k<leadin>[\k<ctx>. \k<obj>]\k<leadout>'
+    content.gsub! /\[\s*(?<ctx>[^\.\[\]%]*)\s*\]\s*(?<obj>[^\[\]]*?)(?<leadout>\s*(;|=>|=|\|))/m, '[\k<ctx>. \k<obj>]\k<leadout>'
+    content.gsub! /<(?<cobj>.*?)>/m, '[\k<cobj>]'
+    content.gsub! /\{(?<ctx>\s*.*?:\s*)\((?<ctxtyp>.*?)\)\*\s*\}/m, '{\k<ctx>\k<ctxtyp>}'
     content.gsub! /FN/, 'mlam'
     content.gsub! /::/, ':'
     self

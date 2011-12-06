f3ee1b54f48926b156da11975c4e3006879fa5be
Mathieu Boespflug <mboes@cs.mcgill.ca>
2011-12-05 23:24:04 -0500

    Remove cutting up input in blocks of different kind.

--- tools/syntax-conv.rb
+++ tools/syntax-conv.rb
@@ -25,30 +25,12 @@ class CompBlock < Block
   end
 end
 
-def comp_level?(s)
-  /(rec|let|schema)\s/ =~ s
-end
-
-def lex(s)
-  s.split(/(;|\.)/)
-end
-
-def classify(shards)
-  shards.map do |s|
-    if comp_level? s then
-      CompBlock.new s
-    else
-      Block.new s
-    end
-  end
-end
-
 if ARGV.length < 1 or ARGV.length > 2 then
     $stderr.puts "Usage: #{File.basename( $0 )} file.bel"
     exit 1
 end
 
 content = File.read ARGV.pop
-blocks = classify(lex content)
-blocks.map! { |b| b.mogrify!.content }
-puts blocks.join
+block = CompBlock.new content
+block.mogrify!
+puts block.content

3f2f53a32d4dbc3971f4860d97bb6c3587c5d5bc
Mathieu Boespflug <mboes@cs.mcgill.ca>
2012-02-01 20:29:03 -0500

    Ground rec/let to beginning of line.

--- tools/syntax-conv.rb
+++ tools/syntax-conv.rb
@@ -43,7 +43,7 @@ class CompBlock < Block
       end
     end
     # rec signature
-    content.gsub_ignore_comments! /(rec|let)\s.*?=/m do |s|
+    content.gsub_ignore_comments! /^(rec|let)\s.*?=/m do |s|
       s.gsub_ignore_comments! /(?<leadin>(::|->|→|:|\})\s*)\(?(?<obj>[^\{\}]*?)\)?\s*\[\s*(?<ctx>.*?)\s*\]/m, '\k<leadin>[\k<ctx>. \k<obj>]'
       s.gsub_ignore_comments! /\{(?<leadin>\s*)(?<ctx>\s*[^\*\{\}:]*?:\s*)(?<ctxtyp>[^\*\{\}:]*?)(?<leadout>\s*)\}/m, '(\k<leadin>\k<ctx>\k<ctxtyp>\k<leadout>)'
       s.gsub_ignore_comments! /(?<leadin>\{\s*)(?<ctx>\s*[^\*\{\}:]*?:\s*)\((?<ctxtyp>[^\*\{\}:]*?)\)\*(?<leadout>\s*\})/m, '\k<leadin>\k<ctx>\k<ctxtyp>\k<leadout>'

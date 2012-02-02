50467548d1e1a00e6ecb74729dac580bf51df640
Mathieu Boespflug <mboes@cs.mcgill.ca>
2012-02-01 23:30:09 -0500

    Turns out '=' is a valid character in an identifier, so have to make regexps a bit more clever.

--- tools/syntax-conv.rb
+++ tools/syntax-conv.rb
@@ -48,14 +48,14 @@ class CompBlock < Block
     end
 
     # rec signature
-    content.gsub_ignore_comments! /^(rec|let)\s.*?=/m do |s|
+    content.gsub_ignore_comments! /^(rec|let)\s.*?[^[[:word:]]]=/m do |s|
       s.gsub_ignore_comments! /(?<leadin>(::|->|→|:|\})\s*)\(?(?<obj>[^\{\}]*?)\)?\s*\[\s*(?<ctx>.*?)\s*\]/m, '\k<leadin>[\k<ctx>. \k<obj>]'
       s.gsub_ignore_comments! /\{(?<leadin>\s*)(?<ctx>\s*[^\*\{\}:]*?:\s*)(?<ctxtyp>[^\*\{\}:]*?)(?<leadout>\s*)\}/m, '(\k<leadin>\k<ctx>\k<ctxtyp>\k<leadout>)'
       s.gsub_ignore_comments! /(?<leadin>\{\s*)(?<ctx>\s*[^\*\{\}:]*?:\s*)\((?<ctxtyp>[^\*\{\}:]*?)\)\*(?<leadout>\s*\})/m, '\k<leadin>\k<ctx>\k<ctxtyp>\k<leadout>'
     end
 
     # rec and top-level let body
-    content.gsub_ignore_comments! /(=.*?;)/m do |s|
+    content.gsub_ignore_comments! /([^[[:word:]]]=.*?;)/m do |s|
       s.gsub_ignore_comments! /\{.*?\}/m do |s|
         s.gsub_ignore_comments! /(?<leadin>(::|->|→|:|\})\s*)\(?(?<obj>[^\{\}]*?)\)?\s*\[\s*(?<ctx>.*?)\s*\]/m, '\k<leadin>[\k<ctx>. \k<obj>]'
       end

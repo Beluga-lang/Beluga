05fd035c62b39c9dd59dbcfd1db1bb7f1303a03f
Mathieu Boespflug <mboes@cs.mcgill.ca>
2012-08-20 14:52:22 -0400

    Use buffer-local write-contents-functions hook to set 'delete-trailing-whitespace.

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,6 +4,6 @@
 ((nil
   (coding . us-ascii)
   (indent-tabs-mode . nil)
-  (eval . (add-hook 'write-file-hooks 'delete-trailing-whitespace)))
+  (eval . (add-hook 'write-contents-functions 'delete-trailing-whitespace)))
  (tuareg-mode
   (compile-command . "omake -R")))

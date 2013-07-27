15e64b8546e9061432a151cfba6116a2697717d7
Brigitte PIENTKA <bpientka@cs.mcgill.ca>
2013-07-27 15:28:35 -0400

    Started reconstruction for subst.vars

--- src/core/whnf.ml
+++ src/core/whnf.ml
@@ -731,6 +731,7 @@ and normSub s = match s with
 
   | Shift _      -> s
   | Dot (ft, s') -> Dot(normFt ft, normSub s')
+  | FSVar ( s , sigma) -> FSVar (s, normSub sigma)
   | SVar (Offset offset, n, s') -> SVar (Offset offset, n, normSub s')
   | SVar (SInst (_n, {contents = Some s}, _cPhi, _cPsi, _cnstr), n, s') ->
     (* cPsi |- s : cPhi  and  cPhi |- Shift n : cPhi'

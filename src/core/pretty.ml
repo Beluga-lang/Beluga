e48173270dd6bb366eb8117df77da0d57a4ebb6e
Brigitte PIENTKA <bpientka@cs.mcgill.ca>
2013-10-09 09:52:43 -0400

    Fixed error in prettty.ml

--- src/core/pretty.ml
+++ src/core/pretty.ml
@@ -487,7 +487,7 @@ module Int = struct
         | LF.FSVar (s_name, (cshift , n), s) ->
             let cs = match cshift with
               | LF.NegCtxShift _ -> "^NegCtxShift _ + " ^ string_of_int n
-              | LF.NoCtxShift _ -> "^NoCtxShift _ + " ^ string_of_int n
+              | LF.NoCtxShift -> "^NoCtxShift _ + " ^ string_of_int n
               | LF.CtxShift _ -> "CtxShift _ + " ^ string_of_int n
             in
           fprintf ppf "$ FSV %s^%s[%a]"
@@ -498,7 +498,7 @@ module Int = struct
         | LF.SVar (c, (cshift , n), s) ->
             let cs = match cshift with
               | LF.NegCtxShift _ -> "^NegCtxShift _ + " ^ string_of_int n
-              | LF.NoCtxShift _ -> "^NoCtxShift _ + " ^ string_of_int n
+              | LF.NoCtxShift -> "^NoCtxShift _ + " ^ string_of_int n
               | LF.CtxShift _ -> "CtxShift _ + " ^ string_of_int n
             in
             (* Ignore CtxShifts for pretty printing *)

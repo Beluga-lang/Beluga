9c9391a52a6cf54260910860d04e5fbdffc1771f
Francisco Ferreira <fferre8@cs.mcgill.ca>
2013-11-25 15:13:40 -0500

    Added support for hardcoded versions (specified in a .version file)

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,9 +1,10 @@
 open Ocamlbuild_plugin
 open Unix
 
+let hardcoded_version_file = ".version"
 let version_file = "src/core/version.ml"
 
-let get_version () =
+let get_version_from_git () =
   try
     let i = open_process_in "git describe --dirty" in
     let l = input_line i in
@@ -14,6 +15,20 @@ let get_version () =
       "Unknown"
   with
   | _ -> "Unknowable"
+
+let hardcoded_version_exists () =
+  Sys.file_exists hardcoded_version_file
+
+let get_hardcoded_version () =
+  let i = open_in hardcoded_version_file in
+  let s = input_line i in
+  close_in i ; s
+
+let get_version () =
+  if hardcoded_version_exists () then
+    get_hardcoded_version ()
+  else
+    get_version_from_git ()
 
 let make_content version = 
   "let beluga_version = \"" ^ version ^ "\"\n"

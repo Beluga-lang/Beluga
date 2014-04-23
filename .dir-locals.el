c9efedadf7aa85e5c3b7c4b72688f7659dd12743
Matthias Puech <puech@cs.mcgill.ca>
2014-04-23 17:19:31 -0400

    OCamlbuild build system: various major enhancements
    * The only visible change of this patch is that the executables
      produced by doing [make] are not in a [bin] directory anymore,
      but at the root.
    * New minimalistic Makefile to drive OCamlbuild
      - launch compilation with make, make all, make clean as usual
      - you can pass it options DEBUG, PROFILE, PARALLEL,
        VERBOSE, WARN_PATTERNS
    * added file all.itarget containing all OCamlbuild targets
    * revamped myocamlbuild.ml to build the version file and
      executables on demand
    * rewritten the _tags files to be more compact and robust
    * removed .dir-locals.el
      (its only use was to set omake as the default compile command
       it seems)
    * added recommended OCamlbuild files to .gitignore

--- .dir-locals.el
+++
@@ -1,9 +0,0 @@
-;;; Directory Local Variables
-;;; See Info node `(emacs) Directory Variables' for more information.
-
-((nil
-  (coding . us-ascii)
-  (indent-tabs-mode . nil)
-  (eval . (add-hook 'before-save-hook 'delete-trailing-whitespace nil t)))
- (tuareg-mode
-  (compile-command . "omake -R")))

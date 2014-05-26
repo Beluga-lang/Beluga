78b37d71a6d4e22da407e7d557ef11624f2b8f53
Andrew Cave <acave1@cs.mcgill.ca>
2014-05-26 15:18:42 -0400

    Removed more uses of CtxShift; tests still pass

--- src/core/subord.ml
+++ src/core/subord.ml
@@ -199,7 +199,7 @@ let thin cD (tP, cPsi) =
             (Shift(NoCtxShift, 0),  CtxVar psi))  (* psi |- shift(noCtx, 0) : psi *)
         else
           ( (* print_string ("Denying that the context variable is relevant to anything in " ^ basisToString basis ^ "\n"); *)
-            (Shift(CtxShift psi, 0),  Null) )  (* psi |- shift(noCtx, 0) : . *)
+            ( EmptySub (*Shift(CtxShift psi, 0) *),  Null) )  (* psi |- shift(noCtx, 0) : . *)
     | DDec(cPsi, TypDecl(name, tA)) ->
         begin match relevant (Whnf.normTyp (tA, Substitution.LF.id)) basis with
           | [] ->

d67d710aa1c2a7b47d8854e62f2ec8eae121a3cd
Andrew Cave <acave1@cs.mcgill.ca>
2014-05-28 11:34:52 -0400

    Fixed output of test case to correspond with printing of empty subst

--- src/core/lfcheck.ml
+++ src/core/lfcheck.ml
@@ -348,8 +348,7 @@ and canAppear cD cPsi head sA loc=
 and checkSub loc cD cPsi1 s1 cPsi1' =
   let rec checkSub loc cD cPsi s cPsi' = match cPsi, s, cPsi' with
     | cPsi, EmptySub, Null -> ()
-    | cPsi, Undefs, _ -> () (* This should have Null domain, but these seem to get used
-			       for strange "strengthenings" *)
+    | cPsi, Undefs, Null -> ()
     | Null, Shift (NoCtxShift, 0), Null -> ()
 
     | cPhi, SVar (Offset offset, (cs, k), s'), cPsi ->

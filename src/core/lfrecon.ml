ed85afc1af652c1d37f862cc97ca76165e0ce38b
Aidan MARCHILDON <amarch23@lab6-7.CS.McGill.CA>
2014-06-23 16:51:50 -0400

    adding error messages from named projections

--- src/core/lfrecon.ml
+++ src/core/lfrecon.ml
@@ -514,8 +514,7 @@ let rec flattenProjPat s conv_list cPsi = match s with
           _ -> raise Not_found
         end in
       let indk = begin try Int.LF.getIndex (Int.LF.BVar k) (recA, Substitution.LF.id) j 1
-                     with _ -> raise (Error (loc, ProjNotFound (cD, cPsi, k,
-                                                         (Int.LF.Sigma recA, Substitution.LF.id))))
+                     with _ -> dprint (fun () -> "Cannot find projection."); raise Not_found
       end
        in
       let s' = flattenProjPat s conv_list cPsi in

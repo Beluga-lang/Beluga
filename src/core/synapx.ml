f83755b09d4203c365507107a7909ddfbbd52607
Andrew Cave <acave1@cs.mcgill.ca>
2015-05-01 17:00:25 -0400

    Removed old unused BranchBox code

--- src/core/synapx.ml
+++ src/core/synapx.ml
@@ -193,16 +193,7 @@ module Comp = struct
   and branch =
     | EmptyBranch of Loc.t * LF.ctyp_decl LF.ctx * pattern
     | Branch of Loc.t * LF.ctyp_decl LF.ctx * LF.ctyp_decl LF.ctx * pattern * exp_chk
-    (* The following two are from the old implementation and will be removed eventually;
-       and replaced by the more general notion of patterns and branches above;
-       it remains currently so we can still use the old parser without modifications
-       -bp *)
-    | BranchBox of Loc.t * LF.ctyp_decl LF.ctx * LF.ctyp_decl LF.ctx
-        * (LF.dctx * branch_pattern * (LF.typ * LF.dctx) option)
 
-    | BranchSBox of Loc.t * LF.ctyp_decl LF.ctx * LF.ctyp_decl LF.ctx
-        * (LF.dctx * LF.sub * LF.dctx option)
-        * exp_chk
 
   (* the definition of branch_pattern will be removed and replaced by the more general notion of patterns;
      it remains currently so we can still use the old parser without modifications -bp *)

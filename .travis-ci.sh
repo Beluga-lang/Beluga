f6620d0e9f49e47e49b1690eb45be09f12e32cbb
David Thibodeau <david.thibodeau@mail.mcgill.ca>
2015-05-14 17:10:22 -0400

    Added zsh as the requirements for travis-ci virtual machine.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -10,7 +10,7 @@ esac
 
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam zsh
 export OPAMYES=1
 export OPAMVERBOSE=1
 echo OCaml version

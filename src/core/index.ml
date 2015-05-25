6afe0354bf3a4394d1b4d1d4600418ff090be90e
Andrew Cave <acave1@cs.mcgill.ca>
2015-05-25 14:41:45 -0400

    Removed unused special case in indexing

--- src/core/index.ml
+++ src/core/index.ml
@@ -633,18 +633,9 @@ and index_meta_spine cvars fcvars = function
 
 let index_meta_typ cvars fcvars = function
   | Ext.Comp.MetaTyp (loc, a, psi) ->
-    begin match a with
-      | Ext.LF.Atom (_ , name, Ext.LF.Nil)
-          when (try ignore (CVar.index_of_name cvars name); true with Not_found -> false) ->
-        let offset = CVar.index_of_name cvars name in
-        let (psi', _ , fcvars1) = index_dctx cvars (BVar.create ()) fcvars psi in
-        let _ = dprint (fun () -> "Indexing TypSub -- turning TypBox into TypSub") in
-          (Apx.Comp.MetaSubTyp (loc, Apx.LF.CtxVar (Apx.LF.CtxOffset offset), psi'), fcvars1)
-      | _ ->
         let (psi', bvars', fcvars') = index_dctx cvars (BVar.create ()) fcvars psi in
         let (a', fcvars'' )         = index_typ cvars bvars' fcvars' a   in
         (Apx.Comp.MetaTyp (loc, a', psi'), fcvars'')
-    end
 
  | Ext.Comp.MetaSubTyp (loc, phi, psi)    ->
       let (psi', _ , fcvars1 ) = index_dctx cvars (BVar.create ()) fcvars psi in

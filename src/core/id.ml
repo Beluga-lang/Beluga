6aea0efc3adc397a79a62e1ea300b8b849b74c37
Agata Murawska <agmu@itu.dk>
2015-06-19 16:56:32 -0400

    Cleanup in mk_name

--- src/core/id.ml
+++ src/core/id.ml
@@ -56,36 +56,27 @@ type name_guide =
   | SomeName of name
   | SomeString of string
 
-let mk_name ?(modules=[]) : name_guide -> name = function
+let mk_name ?(modules=[]) : name_guide -> name =
+
+  let mk_name_helper (nm: string) : name =
+    { modules = modules;
+      string_of_name = nm;
+      was_generated = true;
+      counter = 0} in
+  function
     (* If no {!name} is given, create a new unique {!name}.
        This prevents the problem that when a {!Store.Typ.entry} or {!Store.Term.entry} is
        looked up, we never have to compare a {!string option}.
        This prevents the case where two entries appear to refer to the same name
        because {!None} = {!None}. *)
-  | MVarName (Some vGen)  ->
-        { modules = modules; string_of_name = vGen() ; was_generated = true; counter = 0}
-
-  | MVarName None  ->
-      { modules = modules; string_of_name = Gensym.MVarData.gensym() ;was_generated = true; counter = 0 }
-
-  | PVarName (Some vGen)  ->
-        { modules = modules; string_of_name = vGen() ; was_generated = true; counter = 0 }
-
-  | PVarName None  ->
-      { modules = modules; string_of_name = "#" ^ Gensym.VarData.gensym() ; was_generated = true; counter = 0 }
-
-  | SVarName None ->
-      { modules = modules; string_of_name = Gensym.MVarData.gensym() ; was_generated = true; counter = 0}
-
-  | SVarName (Some vGen) ->
-        { modules = modules; string_of_name = vGen() ; was_generated = true; counter = 0 }
-
-  | BVarName (Some vGen)   ->
-        { modules = modules; string_of_name = vGen() ; was_generated = true; counter = 0}
-
+  | MVarName (Some vGen)
+  | PVarName (Some vGen)
+  | SVarName (Some vGen)
+  | BVarName (Some vGen) -> mk_name_helper (vGen())
+  | MVarName None
+  | SVarName None -> mk_name_helper (Gensym.MVarData.gensym())
+  | PVarName None  -> mk_name_helper ("#" ^ Gensym.VarData.gensym())
+  | BVarName None
+  | NoName     -> mk_name_helper (Gensym.VarData.gensym ())
   | SomeName x  -> x
-
-  | SomeString x ->
-        { modules = modules; string_of_name = x ; was_generated = false; counter = 0 }
-
-  | _     -> { modules = modules; string_of_name = (Gensym.VarData.gensym ())  ; was_generated = true; counter = 0 }
+  | SomeString x -> mk_name_helper x

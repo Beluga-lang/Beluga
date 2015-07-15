e2e5b7f40cd9022907f70d691a0afb5c6bd33993
stheph <steven.thephsourithone@mail.mcgill.ca>
2015-07-15 17:53:12 -0400

    Fixed error in MLam cases for proof_theorem (Second MLam case now covers inductive, no longer returns e instead of e')

--- src/core/latex.ml
+++ src/core/latex.ml
@@ -79,6 +79,20 @@ let rec proof e_ann =
 			end
 	)
 	theorem in
+	let _ = (match e' with
+		| Synann.Comp.Syn _ -> print_string "After proof_theorem: Returned Syn\n"
+		| Synann.Comp.Rec _ -> print_string "After proof_theorem: Returned Rec\n"
+		| Synann.Comp.Fun _ -> print_string "After proof_theorem: Returned Fun\n"
+		| Synann.Comp.Cofun _ -> print_string "After proof_theorem: Returned Cofun\n"
+		| Synann.Comp.MLam _ -> print_string "After proof_theorem: Returned MLam\n"
+		| Synann.Comp.Pair _ -> print_string "After proof_theorem: Returned Pair\n"
+		| Synann.Comp.LetPair _ -> print_string "After proof_theorem: Returned LetPair\n"
+		| Synann.Comp.Let _ -> print_string "After proof_theorem: Returned Let\n"
+		| Synann.Comp.Box _ -> print_string "After proof_theorem: Returned Box\n"
+		| Synann.Comp.Case _ -> print_string "After proof_theorem: Returned Case\n"
+		| Synann.Comp.If _ -> print_string "After proof_theorem: Returned If\n"
+		| Synann.Comp.Hole _ -> print_string "After proof_theorem: Returned Hole\n"
+	) in
 	let _ = proof_case e' in
 	(* let cases = proof_cases e' in	 *)
 	()
@@ -90,12 +104,12 @@ and proof_theorem e = match e with
 		| Synann.Comp.Case (_, _, _, _, _, ttau) ->  ([], e')
 		| _ -> let (tlist, e'') = proof_theorem e' in (tlist, e'')	
 	end
-| Synann.Comp.MLam (_, u, e', cD, (Syntax.Int.Comp.TypPiBox ((Syntax.Int.LF.Decl (_, cU, Int.LF.No)) as cdec, tau), theta)) ->
+| Synann.Comp.MLam (_, u, e', cD, (Syntax.Int.Comp.TypPiBox ((Syntax.Int.LF.Decl (_, cU, _)) as cdec, tau), theta)) ->
 	begin
 		match e' with
-		| Synann.Comp.Case (_, _, _, _, cD', ttau) -> 
-			((TheoremForall (u, cD, cdec))::(TheoremTerm (None, cD', ttau))::[], e)			
-		| _ -> 
+		| Synann.Comp.Case (_, _, _, _, cD', ttau) -> print_string "Case statement\n";
+			((TheoremForall (u, cD, cdec))::(TheoremTerm (None, cD', ttau))::[], e')			
+		| _ ->
 			let (tlist, e'') = proof_theorem e' in ((TheoremForall (u, cD, cdec))::tlist, e'')
 	end	
 | Synann.Comp.Fun (_, n, e', cD, (Int.Comp.TypArr (t1, t2), theta)) ->
@@ -106,50 +120,65 @@ and proof_theorem e = match e with
 		| _ -> 
 			let (tlist, e'') = proof_theorem e' in ((TheoremTerm (Some n, cD, (t1, theta)))::tlist, e'')
 	end
+| Synann.Comp.Syn _ -> raise (LatexException "TestSyn\n")
+| Synann.Comp.Rec _ -> raise (LatexException "TestRec\n")
+| Synann.Comp.Cofun _ -> raise (LatexException "TestCofun\n")
+| Synann.Comp.Pair _ -> raise (LatexException "TestPair\n")
+| Synann.Comp.LetPair _ -> raise (LatexException "TestLetPair\n")
+| Synann.Comp.Let _ -> raise (LatexException "TestLet\n")
+| Synann.Comp.Box _ -> raise (LatexException "TestBox\n")
+| Synann.Comp.Case _ -> raise (LatexException "TestCase\n")
+| Synann.Comp.If _ -> raise (LatexException "TestIf\n")
+| Synann.Comp.Hole _ -> raise (LatexException "TestHole\n")
 | _ -> raise (LatexException "Non MLam/Fun expression passed to proof_theorem")
 
 
 and proof_case e = match e with(* return scrut + case list *)
 | Synann.Comp.Case (loc, prag, i, branches, cD, ttau) -> proof_branches branches
+| Synann.Comp.Syn _ -> raise (LatexException "Non-case passed to proof_case: Syn\n")
+| Synann.Comp.Fun _ -> raise (LatexException "Non-case passed to proof_case: Fun\n")
+| Synann.Comp.MLam _ -> raise (LatexException "Non-case passed to proof_case: MLam\n")
+| Synann.Comp.Rec _ -> raise (LatexException "Non-case passed to proof_case: Rec\n")
+| Synann.Comp.Cofun _ -> raise (LatexException "Non-case passed to proof_case: Cofun\n")
+| Synann.Comp.Pair _ -> raise (LatexException "Non-case passed to proof_case: Pair\n")
+| Synann.Comp.LetPair _ -> raise (LatexException "Non-case passed to proof_case: LetPair\n")
+| Synann.Comp.Let _ -> raise (LatexException "Non-case passed to proof_case: Let\n")
+| Synann.Comp.Box _ -> raise (LatexException "Non-case passed to proof_case: Box\n")
+| Synann.Comp.If _ -> raise (LatexException "Non-case passed to proof_case: If\n")
+| Synann.Comp.Hole _ -> raise (LatexException "Non-case passed to proof_case: Hole\n")
 (* | Int.Comp.Case (loc, prag, Int.Comp.Ann (Int.Comp.Box (_, (l,cM)), (Int.Comp.TypBox (_, mT) as tau0_sc)), branches), (tau, t)) -> *)
-
-
-| _ -> raise (LatexException "Non Case argument passed to proof_case")
 
 and proof_branches b = List.map proof_branch b
 
 and proof_branch b = match b with
+| Synann.Comp.EmptyBranch (loc, cD1', pat, t1) -> print_string "TestEmptyBranch\n"
+| Synann.Comp.Branch (loc, cD1', _cG, Synann.Comp.PatMetaObj (loc', mO, ttau'), t1, e1) -> print_string "TestBranch\n"
 | Synann.Comp.Branch (loc, cD1', cG1, pat, t1, e1) -> proof_pattern pat
-(* | Synann.Comp.EmptyBranch (loc, cD1', pat, t1) -> *)
-(* | Synann.Comp.Branch (loc, cD1', _cG, Synann.Comp.PatMetaObj (loc', mO, ttau'), t1, e1) -> *)
 
 and proof_pattern pat = match pat with (* this returns steps *)
-| Synann.Comp.PatMetaObj (loc, mO, ttau) ->
-	proof_metaobj mO
-(* | Synann.Comp.PatEmpty (loc, cPsi, ttau) -> *)
-(* | Synann.Comp.PatConst (loc, c, pat_spine, ttau) -> *)
-(* | Synann.Comp.PatVar (loc, k, ttau) -> *)
-(* | Synann.Comp.PatPair (loc, pat1, pat2, ttau) -> *)
-(* | Synann.Comp.PatTrue (loc, ttau) -> *)
-(* | Synann.Comp.PatFalse (loc, ttau) -> *)
-| Synann.Comp.PatAnn (loc, pat, tau, ttau) -> 
-	proof_pattern pat
+| Synann.Comp.PatMetaObj (loc, mO, ttau) -> print_string "TestPatMetaObj\n"; proof_metaobj mO
+| Synann.Comp.PatEmpty (loc, cPsi, ttau) -> print_string "TestPatEmpty\n"
+| Synann.Comp.PatConst (loc, c, pat_spine, ttau) -> print_string "TestPatConst\n"
+| Synann.Comp.PatVar (loc, k, ttau) -> print_string "TestPatVar\n"
+| Synann.Comp.PatPair (loc, pat1, pat2, ttau) -> print_string "\nTestPatPair\n"
+| Synann.Comp.PatTrue (loc, ttau) -> print_string "TestPatTrue\n"
+| Synann.Comp.PatFalse (loc, ttau) -> print_string "TestPatFalse\n"
+| Synann.Comp.PatAnn (loc, pat, tau, ttau) -> print_string "TestPatAnn\n"; proof_pattern pat
 
 and proof_metaobj (loc, mO) = match mO with (* this returns steps *)
-| Synann.LF.ClObj (phat, tM, ttau) ->
+| Synann.LF.ClObj (phat, tM, ttau) -> print_string "TestClObj\n";
 	begin
 		match tM with
-		| Synann.LF.MObj (tM', ttau') -> proof_normal tM'
-		(* | SObj (tM, ttau') -> *)
-		(* | PObj (h, ttau') -> *)
+		| Synann.LF.MObj (tM', ttau') -> print_string "TestMObj\n"; proof_normal tM'
+		| Synann.LF.SObj (tM, ttau') -> print_string "TestSObj\n"
+		| Synann.LF.PObj (h, ttau') -> print_string "TestPObj\n"
 	end		
-(* | Synann.LF.CObj (cPsi, ttau) -> *)
-
-(* | Synann.LF.MV (u, ttau) -> *)
+| Synann.LF.CObj (cPsi, ttau) -> print_string "TestCObj\n" 
+| Synann.LF.MV (u, ttau) -> print_string "TestMV\n"
 
 and proof_normal tM = match tM with
 | Synann.LF.Root (_, h, tS, sA) -> proof_head h; proof_spine tS
-| Synann.LF.Lam (_, name, tM'', sA) -> print_string "TestLam\n"
+| Synann.LF.Lam (_, name, tM'', sA) -> print_string (sprintf "\\%s. %s" (R.render_name name) "NORM\n")
 (* | Synann.LF.Clo (tM'', tS) *)
 (* | Synann.LF.Tuple (_, tup) *) 
 

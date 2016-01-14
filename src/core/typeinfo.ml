868c490b687e2681818f7b3252c3bf6bd32baa2e
Steven Thephsourinthone <steven.thephsourinthone@mail.mcgill.ca>
2016-01-14 16:48:30 -0500

    Writing computation level cases (up to Cofun)

--- src/core/typeinfo.ml
+++ src/core/typeinfo.ml
@@ -21,8 +21,8 @@ module Annot = struct
   }
 
   let store     = Hashtbl.create 0
-  let add (l : Loc.t) (e : entry) =
-		  Hashtbl.replace store l e
+  let add (l : Loc.t) (t : string) =
+		  Hashtbl.replace store l (mk_entry t)
   let get       = Hashtbl.find store
   let to_string (e : entry) = e.typ
   let clear ()  = Hashtbl.clear store
@@ -72,104 +72,156 @@ module Annot = struct
       output_int pp (pos.pos_cnum + 1)
     end
 
-  (* Pairing locations between external and internal syntax *)
-
-  let annotate_sgn_typ loc tK =
-    let tK_str = P.kindToString LF.Null (tK, LF.EmptySub) in
-    add loc (mk_entry tK_str)
-
-  let annotate_sgn_const loc tA =
-    let tA_str = P.typToString LF.Empty LF.Null (tA, LF.EmptySub) in
-    add loc (mk_entry tA_str)
-
 end
 
 module LF = struct
   open Syntax.Int
-  type entry = {
-    ctx : LF.mctx;
-    psi : LF.dctx;
-    tc : LF.tclo
-  }
-
-  let mk_entry (c : LF.mctx) (p : LF.dctx) (t : LF.tclo) : entry =
-  {
-    ctx = c;
-    psi = p;
-    tc = t
-  }
-
-  let store         = Hashtbl.create 0
-  let add (l : Loc.t) (e : entry) (s : string) =
-    if l <> Loc.ghost
-      then begin
-	(* dprint (fun () -> "[TypeInfo.LF] Entry of " ^ P.typToString e.ctx e.psi e.tc ^ " added at: \n" ^ Syntax.Loc.to_string l ^ "\n"); *)
-	Annot.add l (Annot.mk_entry ((* s ^ " :: " ^ *) P.typToString e.ctx e.psi e.tc));
-	Hashtbl.add store l e
-      end else ()
-
-  let get           = Hashtbl.find store
-  let clear ()      = Hashtbl.clear store
 
 end
 
 module Comp = struct
+  open Syntax.Int.Comp
+
+  module S = Substitution
+  module I = Syntax.Int.LF
+  module C = Whnf
+
+  module SELF = Syntax.Ext.LF
+  module SEComp = Syntax.Ext.Comp
+
+  let annotate_comp_exp_chk cD (cG, cIH) eInt eExt ttau = match (eInt, eExt, ttau) with
+    | (Rec (_, f, eInt'), eExt', (tau, t)) ->
+       annotate_comp_exp_chk
+	 cD (I.Dec (cG, CTypDecl (f, TypClo (tau, t))), (Total.shift cIH)) eExt' eInt' ttau
+
+    | (Fun (_, x, eInt'), SEComp.Fun (loc, _, eExt'), (TypArr (tau1, tau2), t)) ->
+       annotate_comp_exp_chk
+	 cD (I.Dec (cG, CTypDecl (x, TypClo (tau1, t))), (Total.shift cIH)) eExt' eInt' (tau2, t);
+       Annot.add loc (P.subCompTypToString cD ttau)
+
+    | (Cofun (_, bs1), SEComp.Cofun (loc, bs2), (TypCobase (l, cid, sp), t)) ->
+       let f = fun (CopatApp (_, dest1, csp1), eInt') (SEComp.CopatApp (loc', _, _), eExt') ->
+	 (let ttau' = synObs cD csp1 ((CompDest.get dest).CompDest.typ, Whnf.m_id) ttau in
+	 annotate_comp_exp_chk cD (cG, cIH) eInt' eExt' ttau';
+	 Annot.add loc' (P.subCompTypToString cD ttau'))
+       in
+       List.iter2 f bs1 bs2;
+       Annot.add loc (P.subCompTypToString cD ttau)
+
+    |
+
+module Comp = struct
   open Syntax.Int
-  type entry = {
-    ctx : LF.mctx;
-    tc: Comp.tclo
-  }
+  module SynE = Syntax.Ext
 
-  let mk_entry (c : LF.mctx) (t : Comp.tclo) : entry =
-  {
-    ctx = c;
-    tc = t
-  }
+  (* e : Syntax.Int.Comp.exp_chk, e' : Syntax.Ext.Comp.exp_chk *)
+  (* Syntax.Int is open *)
+  let annotate_comp_exp_chk cD (cG, cIH) e1 e2 ttau = match (e1, e2, ttau) with
+    | (Comp.Rec (_, f, e1'), e2', (tau, t)) ->
+       annotate_comp_exp_chk
+	 cD (LF.Dec (cG, Comp.CTypDecl (f, Comp.TypClo (tau, t))), (Total.shift cIH))
+	 e1' e2' ttau
 
-  let store         = Hashtbl.create 0
+    | (Comp.Fun (_, x, e1'), SynE.Comp.Fun (loc, _, e2'), (TypArr (tau1, tau2), t)) ->
+       annotate_comp_exp_chk
+	 cD (LF.Dec (cG, Comp.CTypDecl (x, Comp.TypClo (tau1, t))), (Total.shift cIH))
+	 e1' e2' (tau2, t);
+       Annot.add loc (mk_entry (P.subCompTypToString cD ttau))
 
-  let add (l : Loc.t) (e : entry) (s : string) = if l <> Loc.ghost
-				    then
-				      begin
-				      (* dprint (fun () -> "[TypeInfo.Comp] Entry of " ^ P.subCompTypToString e.ctx e.tc ^ " added at: \n" ^ Syntax.Loc.to_string l ^ "\n"); *)
-				      Annot.add l (Annot.mk_entry ((* s ^ " :: " ^  *)P.subCompTypToString e.ctx e.tc)) ;
-				      Hashtbl.add store l e
-				      end
-				    else
-				      ()
+    | (Comp.Cofun (_, bs1), SynE.Comp.Cofun (loc, bs2), (Comp.TypCobase (_, cid, sp), t)) ->
+       let f = fun (Comp.CopatApp (_, dest1, csp1, e1')
+		   , SynE.Comp.CopatApp (_, dest2, csp1, e2')) ->
+	 let ttau' = synObs cD csp1 ((CompDest.get dest).CompDest.typ, Whnf.m_id) ttau
+	 in annotate_comp_exp_chk cD (cG, cIH) e1' e2' ttau'
+       in
+       List.iter f bs;
+       Annot.add loc (mk_entry (P.subCompTypToString cD ttau))
 
-  let get           = Hashtbl.find store
+    | (Comp.MLam (_, u1, e1'), e2', (Comp.TypPiBox(LF.Decl (_,cU,Int.LF.Maybe) as cdec,tau),t)) ->
+       annotate_comp_exp_chk
+	 (extend_mctx cD (u1, cdec, t))
+	 (C.cnormCtx (cG, LF.MShift 1), C.cnormCtx (cIH, LF.MShift 1))
+	 e1' e2' (tau, C.mvar_dot1 t)
 
-  let clear ()      = Hashtbl.clear store
+    | (Comp.MLam (_, u1, e1'), SynE.Comp.MLam (loc, u2, e2'), (Comp.TypPiBox(cdec,tau),t)) ->
+       annotate_comp_exp_chk
+	 (extend_mctx cD (u1, cdec, t))
+	 (C.cnormCtx (cG, LF.MShift 1), C.cnormCtx (cIH, LF.MShift 1))
+	 e1' e2' (tau, C.mvar_dot1 t);
+       Annot.add loc (mk_entry (P.subCompTypToString cD ttau))
+
+    | (Comp.Pair(_,e1a,e1b), SynE.Comp.Pair(loc,e2a,e2b), (Comp.TypCross (tau1, tau2), t)) ->
+       annotate_comp_exp_chk cD (cG, cIH) e1a e2a (tau1, t);
+       annotate_comp_exp_chk cD (cG, cIH) e1b e2b (tau2, t);
+       Annot.add loc (mk_entry (P.subCompTypToString cD ttau))
+
+    | (Comp.Let (_, i1, (x1, e1')), SynE.Comp.Let (loc, i2, (x2, e2')), (tau, t)) ->
+       let (_, tau', t') = annotate_comp_exp_syn cD (cG, cIH) i1 in
+       let (tau', t') = C.whnfCTyp (tau', t') in
+       let cG' = LF.Dec (cG, Comp.CTypDecl (x1, Comp.TypClo (tau', t'))) in
+       annotate_comp_exp_chk cD (cG', Total.shift cIH) e1' e2' (tau, t);
+       Annot.add loc (mk_entry (P.subCompTypToString cD ttau))
+
+    | (Comp.LetPair (_,i1,(x1,y1,e1')), SynE.Comp.LetPair (loc,i2,(x2,y2,e2')), (tau, t)) ->
+       let (_, tau', t') = syn cD (cG, cIH) i1 in
+       let (tau', t') = C.cwhnfCTyp (tau', t') in
+       begin
+	 match (tau', t') with
+	 | (Comp.TypCross (tau1, tau2), t') ->
+	    let cG' = LF.Dec (LF.Dec (cG, Comp.CTypDecl (x1, Comp.TypClo (tau1, t'))),
+			      Comp.CTypDecl (y1, Comp.TypClo (tau2, t')))
+	    in
+	    annotate_comp_exp_chk cD (cG', (Total.shift (Total.shift cIH))) e1' e2' (tau, t);
+	    Annot.add loc (mk_entry (P.subCompTypToString cD ttau))
+	 | _ -> raise (Error.Violation "Case scrutinee not of boxed type.")
+       end
+
+    | (Comp.Box (_, cM1), SynE.Comp.Box (loc, cM2), (Comp.TypBox (l, mT), t)) ->
+       begin
+	 try
+	   (* LF.annotateMetaObj cD cM1 (mT, t); *)
+	   Annot.add loc (mk_entry (P.subCompTypToString cD ttau))
+	 with Whnf.FreeMVar (LF.FMVar (u, _)) ->
+	   raise (Error.Violation ("Free meta-variable " ^ (Id.render_name u)))
+       end
+
+    | (Comp.Case (_, prag1,
+		  Comp.Ann (Comp.Box (_, (l1,cM1)), (Comp.TypBox (_, mT1) as tau0_sc)), branches1),
+       SynE.Comp.Case (loc, prag2, SynE.Comp.Ann (SynE.Comp.Box (_, (l2, cM2)),
+			    (Syntax.Ext.Comp.TypBox (_, mT2) as tau0_sc)), branches2),
+       (tau, t)) ->
+       let (total_pragma, tau_sc, projOpt) =
+	 begin
+	   match cM1 with
+	   | Comp.ClObj (_, Comp.MObj (Comp.Root (_, Comp.PVar (x,s), _)))
+	   | Comp.ClObj (_, Comp.PObj (Comp.PVar (x,s))) ->
+	      let order =
+		if !Total.enabled && is_ind_MObj cD x then
+		  Ind
+
+  and synObs cD csp ttau1 ttau2 = match (csp, ttau1, ttau2) with
+    | (CopatNil loc, (TypArr (tau1, tau2), theta), (tau', theta')) ->
+        if Whnf.convCTyp (tau1, theta) (tau', theta') then
+          (tau2, theta)
+        else
+          raise (Error (loc, TypMismatch (cD, (tau1, theta), (tau',theta'))))
+    | (CopatApp (loc, dest, csp'), (TypArr (tau1, tau2), theta), (tau', theta')) ->
+        if Whnf.convCTyp (tau1, theta) (tau', theta') then
+          synObs cD csp' ((CompDest.get dest).CompDest.typ, Whnf.m_id) (tau2, theta)
+        else
+          raise (Error (loc, TypMismatch (cD, (tau1, theta), (tau',theta'))))
 
 end
 
 module Sgn = struct
   open Syntax.Int
-  type typ_or_kind = Typ of LF.typ | Kind of LF.kind
+  let annotate_sgn_typ loc tK =
+    let tK_str = P.kindToString LF.Null (tK, LF.EmptySub) in
+    Annot.add loc (mk_entry tK_str)
 
-  type entry = {
-    sgn : typ_or_kind
-  }
-
-  let mk_entry (decl : typ_or_kind) : entry =
-    {sgn = decl}
-
-  let store = Hashtbl.create 0
-
-  let add : Loc.t -> entry -> string -> unit =
-    fun l e _ -> if l <> Loc.ghost then begin
-      let s = match e.sgn with
-      | Typ t -> P.typToString LF.Empty LF.Null (t, LF.EmptySub)
-      | Kind k -> P.kindToString LF.Null (k, LF.EmptySub)
-      in
-      Annot.add l (Annot.mk_entry s);
-      Hashtbl.add store l e
-    end
-
-  let get : Loc.t -> entry = Hashtbl.find store
-
-  let clear : unit -> unit = fun () -> Hashtbl.clear store
+  let annotate_sgn_const loc tA =
+    let tA_str = P.typToString LF.Empty LF.Null (tA, LF.EmptySub) in
+    Annot.add loc (mk_entry tA_str)
 
 end
 

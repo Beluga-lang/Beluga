79e1a4406aa4a899a40856e300d557f58206b712
Francisco Ferreira <fco@Grace.local>
2017-04-06 12:14:53 -0400

    Updated travis-ci to use opam packages

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,13 +1,6 @@
 # Edit this for your own project dependencies
 OPAM_DEPENDS="ocamlfind extlib ulex"
 
-case "$OCAML_VERSION" in
-4.00.1) ppa=avsm/ocaml40+opam12 ;;
-4.01.0) ppa=avsm/ocaml41+opam12 ;;
-4.02.1) ppa=avsm/ocaml42+opam12 ;;
-*) echo Unknown $OCAML_VERSION; exit 1 ;;
-esac
-
 export OPAMYES=1
 export OPAMVERBOSE=1
 echo OCaml version
@@ -17,6 +10,7 @@ opam --version
 opam --git-version
 
 opam init
+opam switch "$OCAML_VERSION"
 opam install ${OPAM_DEPENDS}
 eval `opam config env`
 make && ./TEST && (./TEST -- +htmltest) && (./TEST -- +sexp)

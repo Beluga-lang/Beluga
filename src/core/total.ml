257693d555273c3d882b55aae279ce3ffd084927
Brigitte Pientka <bpientka@cs.mcgill.ca>
2018-03-15 21:25:32 +0100

    Reset global unification constraints after generation of wf rec calls

--- src/core/total.ml
+++ src/core/total.ml
@@ -605,6 +605,7 @@ let wf_rec_calls cD cG  =
 		   ^ "\ncG = " ^ P.gctxToString cD cG ^ "\n");   
     let cIH  = gen_rec_calls cD (LF.Empty) (cD, 0) in
     let cIH' = gen_rec_calls' cD cG cIH (cG, 0) in
+    let _ = Unify.resetGlobalCnstrs () in
        (* dprint (fun () -> "generated IH = " ^ ih_to_string cD cG cIH' ^ "\n\n"); *)
       cIH'
     )

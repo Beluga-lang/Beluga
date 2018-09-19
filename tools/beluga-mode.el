Beluga-lang/Beluga

commit 87420b3c1dab48790ed1664112b9b4e59d78b513

beluga-mode: make beluga-{start,stop} interactive

tools/beluga-mode.el
@@ -248,25 +248,26 @@ Regexp match data 0 points to the chars."
 
 (defun beluga--proc ()
   (unless (beluga--proc-live-p beluga--proc) (beluga--start))
-  ;;  (beluga--start)
   beluga--proc)
 
 ;; (defun beluga-buffer ()
 ;;   (process-buffer (beluga--proc)))
 
-(defun beluga--start ()
+(defun beluga-start ()
   "Start an inferior beli process with the -emacs option.
-The process is put into a buffer called \"*beli*\".
+The process is put into a buffer called \"*beluga*\".
 If a previous beli process already exists, kill it first."
-  (beluga--stop)
+  (interactive)
+  (beluga-stop)
   (setq beluga--proc
         (get-buffer-process
          (make-comint "beluga"
 		      beluga-interpreter-name
                       nil "-I" "-emacs" ))))
 
-(defun beluga--stop ()
+(defun beluga-stop ()
   "Stop the beli process."
+  (interactive)
   (when (processp 'beluga--proc)
     (kill-process 'beluga--proc)))
 
@@ -780,7 +781,7 @@ starting position of the short pragma; else, nil."
    `((assoc ";")
      (assoc ",")
      (left ":")
-     (assoc ,@beluga-slim-arrows) 
+     (assoc ,@beluga-slim-arrows)
      (nonassoc " -dummy- "))))          ;Bogus anchor at the end.
 
 (defconst beluga-pattern-fat-arrow

commit bf59020bab486603e756319076dce88fe3765d9d

beluga-mode: properly rename beluga--start to beluga-start
This fixes a bug that slipped between the cracks, introduced in
87420b3c beluga-mode: make beluga-{start,stop} interactive
when I changed the name of beluga--start to beluga-start without
adjusting the call sites.

In principle, the interactive mode would be broken between that commit
and this commit.

tools/beluga-mode.el
@@ -249,7 +249,7 @@ Regexp match data 0 points to the chars."
 (make-variable-buffer-local 'beluga--proc)
 
 (defun beluga--proc ()
-  (unless (beluga--proc-live-p beluga--proc) (beluga--start))
+  (unless (beluga--proc-live-p beluga--proc) (beluga-start))
   beluga--proc)
 
 (defun beluga-start ()
@@ -500,7 +500,7 @@ returned. Else, `nil' is returned."
 (defun beli ()
   "Start beli mode"
   (interactive)
-  (beluga--start))
+  (beluga-start))
 
 (defun beli-cmd (cmd)
   "Run a command in beli"
@@ -621,7 +621,7 @@ Specifically, the following are performed, if necessary:
   - Saving the current buffer.
   - Loading the current buffer into Beluga.
   - Highlighting holes."
-  (beluga--start)
+  (beluga-start)
   (beluga--maybe-save-load-current-buffer)
   (beluga--highlight-holes))
 
@@ -635,7 +635,7 @@ Specifically, the following are performed, if necessary:
   "Load the current file in Beluga Interactive. This command will
 start the interactive mode if necessary and prompt for saving."
   (interactive)
-  (beluga--start)
+  (beluga-start)
   (let ((r (beluga--maybe-save-load-current-buffer)))
     (if r
         (message "%s" r)
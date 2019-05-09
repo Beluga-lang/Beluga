e8a2307f0e02ab785b6b0d7aec2cd5995f6ab7f8
Jacob Errington <git@mail.jerrington.me>
2019-05-09 14:25:20 -0400

    formatting

--- src/beluga/beli.ml
+++ src/beluga/beli.ml
@@ -1,10 +1,10 @@
 open Format
-  
+
 module Options = struct
-	let readline = ref false
+  let readline = ref false
   let ledit = ref true
-	let emacs = ref false
-	let interrupt_count = ref 0
+  let emacs = ref false
+  let interrupt_count = ref 0
 end
 
 exception Invalid_Arg
@@ -32,26 +32,25 @@ let init_repl ppf =
 let rec loop ppf =
   begin
     try
-      if !Options.emacs then ()
-      else fprintf ppf "# ";
+      if not !Options.emacs then fprintf ppf "# ";
       pp_print_flush ppf ();
       let input = read_line () in
-        match Command.is_command input with
-          | `Cmd cmd ->
-            Command.do_command ppf cmd
-          | `Input input ->
-              let sgn = Parser.parse_string ~name:"<interactive>" ~input:input Parser.sgn in
-              let sgn'= begin match Recsgn.recSgnDecls sgn with
-		| sgn', None -> sgn'
-		| _, Some _ ->
-		  raise (Abstract.Error (Syntax.Loc.ghost, Abstract.LeftoverVars))
-	      end
-	      in
-              if !Debug.chatter <> 0 then
-                List.iter (fun x -> let _ = Pretty.Int.DefaultPrinter.ppr_sgn_decl x in ()) sgn'
+      match Command.is_command input with
+      | `Cmd cmd ->
+         Command.do_command ppf cmd
+      | `Input input ->
+         let sgn = Parser.parse_string ~name:"<interactive>" ~input:input Parser.sgn in
+         let sgn'=
+           match Recsgn.recSgnDecls sgn with
+           | sgn', None -> sgn'
+           | _, Some _ ->
+              raise (Abstract.Error (Syntax.Loc.ghost, Abstract.LeftoverVars))
+         in
+         if !Debug.chatter <> 0 then
+           List.iter (fun x -> let _ = Pretty.Int.DefaultPrinter.ppr_sgn_decl x in ()) sgn'
     with
       | End_of_file -> exit 0
-      | Sys.Break -> 
+      | Sys.Break ->
           if !Options.interrupt_count = 2 then begin fprintf ppf "@\n@."; exit 0 end else begin
           incr Options.interrupt_count;
           fprintf ppf "Interrupted (press %d more times to quit).@." (3 - !Options.interrupt_count) end
@@ -66,40 +65,48 @@ let run args =
   let files = process_options args in
   let _ = Debug.pipeDebug := true in
 
-  (* If readline wrapper exists, replace current process with a call
-     to it and ask it to run us, wrapped. Line editing is then
-     available. Otherwise don't bother. *)
-  if !Options.ledit && (not !Options.emacs) then begin
-    try
-      let (_,pre, post) = Array.fold_right 
-        (fun x (f, pre, post) -> 
-            if f then (f, x::pre, post)
-            else if x = "-I" then (true, pre, post) 
-            else (false, pre, x::post)) Sys.argv (false, [], []) in
-      let args = "ledit" :: (pre @ ("-I"::"-ledit"::post)) in
-      Unix.execvp "ledit" (Array.of_list args)
-    with Unix.Unix_error _ -> ()
-  end else if !Options.readline && (not !Options.emacs) then begin
-    try
-      let (_,pre, post) = Array.fold_right 
-        (fun x (f, pre, post) -> 
-            if f then (f, x::pre, post)
-            else if x = "-I" then (true, pre, post) 
-            else (false, pre, x::post)) Sys.argv (false, [], []) in
-      let args = "rlwrap" :: (pre @ ("-I"::"-ledit"::post)) in
-      Unix.execvp "rlwrap" (Array.of_list args)
-    with Unix.Unix_error _ -> ()
-  end;
+  if not !Options.emacs then
+    begin
+      (* If readline wrapper exists, replace current process with a call
+         to it and ask it to run us, wrapped. Line editing is then
+         available. Otherwise don't bother. *)
+      if !Options.ledit then
+        begin
+          try
+            let (_,pre, post) =
+              Array.fold_right
+                (fun x (f, pre, post) ->
+                  if f then (f, x::pre, post)
+                  else if x = "-I" then (true, pre, post)
+                  else (false, pre, x::post)) Sys.argv (false, [], []) in
+            let args = "ledit" :: (pre @ ("-I"::"-ledit"::post)) in
+            Unix.execvp "ledit" (Array.of_list args)
+          with Unix.Unix_error _ -> ()
+        end
+      else if !Options.readline then
+        begin
+          try
+            let (_,pre, post) =
+              Array.fold_right
+                (fun x (f, pre, post) ->
+                  if f then (f, x::pre, post)
+                  else if x = "-I" then (true, pre, post)
+                  else (false, pre, x::post)) Sys.argv (false, [], []) in
+            let args = "rlwrap" :: (pre @ ("-I"::"-ledit"::post)) in
+            Unix.execvp "rlwrap" (Array.of_list args)
+          with Unix.Unix_error _ -> ()
+        end
+    end;
 
   if List.length files = 1 then
     try
       let arg = List.hd files in
       let sgn = Parser.parse_file ~name:arg Parser.sgn in
       let sgn' = begin match Recsgn.recSgnDecls sgn with
-	| sgn', None -> sgn'
-	| _, Some _ ->
-	  raise (Abstract.Error (Syntax.Loc.ghost, Abstract.LeftoverVars))
-      end 
+    | sgn', None -> sgn'
+    | _, Some _ ->
+      raise (Abstract.Error (Syntax.Loc.ghost, Abstract.LeftoverVars))
+      end
       in
         if !Debug.chatter <> 0 then
           List.iter (fun x -> let _ = Pretty.Int.DefaultPrinter.ppr_sgn_decl x in ()) sgn';

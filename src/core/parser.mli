758f16f74faabc4a85e861b7c20238dea9e6b9a7
Jacob Errington <git@mail.jerrington.me>
2019-05-13 14:35:23 -0400

    refactor interactive, command, parser
    Refactor `fill` in command.ml:
    * use higher-order functions for looking up the hole and verifying
      that it's a computation hole.
    * parse the expression directly instead of via constructing a
      signature and using partial pattern matching.
    * use Interactive.elaborate_exp.
    
    interactive.ml:
    * Define interactive.mli interface.
    * Remove unused functions from interactive.ml.
    * Add elaborate_exp and elaborate_exp' to interactive.ml to go from
      external syntax directly to internal syntax, when Delta and Gamma
      are known. This is useful for elaborating terms typed by the user in
      an interactive session, e.g. in the `fill` command and in Harpoon.
    * Remove unused argument `tau` from `branchCovGoals`
    * Rename & refactor replaceHole. Now it takes the hole and ID as input
      rather than do the lookup itself.
    
    parser:
    * Export `cmp_exp_chk` parser (used by `fill`)
    * Command exported productions.

--- src/core/parser.mli
+++ src/core/parser.mli
@@ -4,12 +4,21 @@ module Grammar : Camlp4.Sig.Grammar.Static
   with module Loc   = Lexer.Loc
   and  module Token = Lexer.Token
 
+(***** Exported grammar productions *****)
+
 (** Grammar entry for an entire LF Signature *)
 val sgn : Ext.Sgn.sgn Grammar.Entry.t
 
+(** Grammar entry for a computation type. *)
 val cmp_typ : Ext.Comp.typ Grammar.Entry.t
 
+(** Grammar entry for a Harpoon command. *)
 val harpoon_command : Ext.Harpoon.command Grammar.Entry.t
+
+(** Grammar entry for a checkable computation expression. *)
+val cmp_exp_chk : Ext.Comp.exp_chk Grammar.Entry.t
+
+(***** Parsing functions *****)
 
 (** Parse a stream and return a signature *)
 val parse_stream :

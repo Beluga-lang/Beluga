87b54071dd35743abb99b6e71ff7a4223070484f
Jacob Errington <git@mail.jerrington.me>
2019-05-23 13:50:24 -0400

    order: formatting; helper for numeric order
    Conversion from a numeric ordering is a trivial renaming of
    constructors. The whole file was indented, so I undid that.

--- src/core/order.ml
+++ src/core/order.ml
@@ -1,22 +1,29 @@
+module Comp = Syntax.Ext.Comp
 
-  type order =	       	                (* Orders                     *)
-      Arg of int			(* O ::= x                    *)
-    | Lex of order list                 (*     | {O1 .. On}           *)
-    | Simul of order list               (*     | [O1 .. On]           *)
+type order =	       	                (* Orders                     *)
+    Arg of int			(* O ::= x                    *)
+  | Lex of order list                 (*     | {O1 .. On}           *)
+  | Simul of order list               (*     | [O1 .. On]           *)
 
-  (* Mutual dependencies in call patterns:                            *)
-  (* A call pattern   (a1 P1) .. (ai Pi) .. (an Pn)   expresses       *)
-  (* that the proof of ai can refer to                                *)
-  (*   ih a1 .. ai, as long as the arguments are strictly smaller     *)
-  (* and to                                                           *)
-  (*   ih a(i+1) .. an as long as the arguments are smaller or equal  *)
-  (* then the ones of ai.                                             *)
+let rec of_numeric_order (o : Comp.numeric_order) : order =
+  match o with
+  | Comp.Arg n -> Arg n
+  | Comp.Lex xs -> Lex (List.map of_numeric_order xs)
+  | Comp.Simul xs -> Simul (List.map of_numeric_order xs)
 
-  type mutual =		                (* Mutual dependencies        *)
-      Empty				(* C ::= .                    *)
-    | LE of Id.cid_prog * mutual	(*     |  <= (a) C            *)
-    | LT of Id.cid_prog * mutual	(*     |  > (a) C             *)
 
-  type dec =                           (* Termination declaration    *)
-      Dec of order * mutual            (* Dec ::= (O, C)            *)
+(* Mutual dependencies in call patterns:                            *)
+(* A call pattern   (a1 P1) .. (ai Pi) .. (an Pn)   expresses       *)
+(* that the proof of ai can refer to                                *)
+(*   ih a1 .. ai, as long as the arguments are strictly smaller     *)
+(* and to                                                           *)
+(*   ih a(i+1) .. an as long as the arguments are smaller or equal  *)
+(* then the ones of ai.                                             *)
 
+type mutual =		                (* Mutual dependencies        *)
+    Empty				(* C ::= .                    *)
+  | LE of Id.cid_prog * mutual	(*     |  <= (a) C            *)
+  | LT of Id.cid_prog * mutual	(*     |  > (a) C             *)
+
+type dec =                           (* Termination declaration    *)
+    Dec of order * mutual            (* Dec ::= (O, C)            *)

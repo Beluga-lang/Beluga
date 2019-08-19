d699dccad9d974590648a6ee4a017580bef6f175
Junyoung Clare Jang <jjc9310@gmail.com>
2019-08-19 14:19:40 -0400

    Refactor shell programs with shellcheck

--- tools/hash_to_percent.sh
+++ tools/hash_to_percent.sh
@@ -1,11 +1,15 @@
-find $1 -type f -name "*.bel" | while read bel; do
-	TMP_FILE=`mktemp /tmp/config.XXXXXXXXXX`	
-	sed -e 's/#infix/%infix/g' $bel > $TMP_FILE && mv $TMP_FILE $bel
-	sed -e 's/#prefix/%prefix/g' $bel > $TMP_FILE && mv $TMP_FILE $bel
-	sed -e 's/#opts/%opts/g' $bel > $TMP_FILE && mv $TMP_FILE $bel
-	sed -e 's/#open/%open/g' $bel > $TMP_FILE && mv $TMP_FILE $bel
-	sed -e 's/#assoc/%assoc/g' $bel > $TMP_FILE && mv $TMP_FILE $bel
-	sed -e 's/%opts +strengthen;//g' $bel > $TMP_FILE && mv $TMP_FILE $bel
-	sed -e 's/%opts -strengthen;/%nostrengthen/g' $bel > $TMP_FILE && mv $TMP_FILE $bel
+#!/usr/bin/env bash
+
+set -eu
+
+find "$1" -type f -name "*.bel" | while read -r bel; do
+	TMP_FILE=$(mktemp /tmp/config.XXXXXXXXXX)
+	sed -e 's/#infix/%infix/g' "${bel}" > "${TMP_FILE}" && mv "${TMP_FILE}" "${bel}"
+	sed -e 's/#prefix/%prefix/g' "${bel}" > "${TMP_FILE}" && mv "${TMP_FILE}" "${bel}"
+	sed -e 's/#opts/%opts/g' "${bel}" > "${TMP_FILE}" && mv "${TMP_FILE}" "${bel}"
+	sed -e 's/#open/%open/g' "${bel}" > "${TMP_FILE}" && mv "${TMP_FILE}" "${bel}"
+	sed -e 's/#assoc/%assoc/g' "${bel}" > "${TMP_FILE}" && mv "${TMP_FILE}" "${bel}"
+	sed -e 's/%opts +strengthen;//g' "${bel}" > "${TMP_FILE}" && mv "${TMP_FILE}" "${bel}"
+	sed -e 's/%opts -strengthen;/%nostrengthen/g' "${bel}" > "${TMP_FILE}" && mv "${TMP_FILE}" "${bel}"
 done
 
--- tools/update_examples.sh
+++ tools/update_examples.sh
@@ -1,10 +1,14 @@
-find $1 -type f -name "*.bel" | while read bel; do
-	TMP_FILE=`mktemp /tmp/config.XXXXXXXXXX`	
-	sed -e 's/\[\s*\.\s*/[ |- /g' $bel > $TMP_FILE && mv $TMP_FILE $bel
-	sed -e 's/\[\s*\([a-z]\)\s*\.\s*/[\1 |- /g' $bel > $TMP_FILE && mv $TMP_FILE $bel
-	sed -e 's/\[\(\w*[,]\w*\)\.\s*/[\1 |- /g' $bel > $TMP_FILE && mv $TMP_FILE $bel
-	sed -e 's/\[\(\(\s*\w*[,:(){}\&]\?'"'"'\?\s\?\(->\)\?\((\([\]\?\w*\s*[,:{}()&\]*'"'"'\?\(->\)\?\(\.\.\)\?\)*)\)*\)*[)}]\?\s*\)\.\([^.]\)/[\1 |- \8/g' $bel > $TMP_FILE && mv $TMP_FILE $bel
-	sed -e 's/^\(\(\s*\w*[,:(){}\&]\?'"'"'\?\s\?\(->\)\?\((\([\]\?\w*\s*[,:{}()&\]*'"'"'\?\(->\)\?\(\.\.\)\?\)*)\)*\)*[)}]\?\s*\)\.\([^.].*\)\]/\1 |- \8]/g' $bel > $TMP_FILE && mv $TMP_FILE $bel
-	sed -e 's/\(\\\w\+\s*\)|-/\1\./g' $bel > $TMP_FILE && mv $TMP_FILE $bel
+#!/usr/bin/env bash
+
+set -eu
+
+find "$1" -type f -name "*.bel" | while read -r bel; do
+	TMP_FILE=$(mktemp /tmp/config.XXXXXXXXXX)
+	sed -e 's/\[\s*\.\s*/[ |- /g' "${bel}" > "${TMP_FILE}" && mv "${TMP_FILE}" "${bel}"
+	sed -e 's/\[\s*\([a-z]\)\s*\.\s*/[\1 |- /g' "${bel}" > "${TMP_FILE}" && mv "${TMP_FILE}" "${bel}"
+	sed -e 's/\[\(\w*[,]\w*\)\.\s*/[\1 |- /g' "${bel}" > "${TMP_FILE}" && mv "${TMP_FILE}" "${bel}"
+	sed -e 's/\[\(\(\s*\w*[,:(){}\&]\?'"'"'\?\s\?\(->\)\?\((\([\]\?\w*\s*[,:{}()&\]*'"'"'\?\(->\)\?\(\.\.\)\?\)*)\)*\)*[)}]\?\s*\)\.\([^.]\)/[\1 |- \8/g' "${bel}" > "${TMP_FILE}" && mv "${TMP_FILE}" "${bel}"
+	sed -e 's/^\(\(\s*\w*[,:(){}\&]\?'"'"'\?\s\?\(->\)\?\((\([\]\?\w*\s*[,:{}()&\]*'"'"'\?\(->\)\?\(\.\.\)\?\)*)\)*\)*[)}]\?\s*\)\.\([^.].*\)\]/\1 |- \8]/g' "${bel}" > "${TMP_FILE}" && mv "${TMP_FILE}" "${bel}"
+	sed -e 's/\(\\\w\+\s*\)|-/\1\./g' "${bel}" > "${TMP_FILE}" && mv "${TMP_FILE}" "${bel}"
 done
 

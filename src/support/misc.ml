7a69fc580f9c03338b2014753fd7853f0e678ecd
Jacob Errington <git@mail.jerrington.me>
2019-10-22 17:44:03 -0400

    support: generators
    - Add a general method for turning a generator for characters into a
      line-base generator for strings.
      It assumes unix line endings '\n', so it is not portable.
    - Add a conversion from input channels to line-based string
      generators.
      It essentially repeatedly calls input_line.
    - Add a function for sequencing a list of generators together.

--- src/support/misc.ml
+++ src/support/misc.ml
@@ -117,6 +117,13 @@ module Gen = struct
     else
       None
 
+  let of_in_channel_lines chan =
+    fun () ->
+    try
+      Some (input_line chan)
+    with
+    | _ -> None
+
   let of_in_channel ?(buffer_size = 1024) chan =
     let bs = Bytes.create buffer_size in
     let count = ref 0 in
@@ -139,4 +146,38 @@ module Gen = struct
        else
          next ()
     | () -> failwith "impossible"
+
+  (** Not portable. Will not work with windows line endings. *)
+  let line_generator ?(buffer_size = 2048) (g : char Gen.t) : string Gen.t =
+    let bs = Bytes.create buffer_size in
+    let finished = ref false in
+    function
+    | _ when !finished -> None
+    | _ ->
+       let got_nl = ref false in
+       let i = ref 0 in
+       while not !got_nl do
+         let c = g () in
+         match c with
+         | None ->
+            got_nl := true;
+            finished := true
+         | Some c ->
+            Bytes.set bs !i c;
+            incr i;
+            got_nl := c = '\n'
+       done;
+       Some (Bytes.sub_string bs 0 !i)
+
+  let sequence (gs : 'a Gen.t list) : 'a Gen.t =
+    let rgs = ref gs in
+    let rec go () =
+      match !rgs with
+      | [] -> None
+      | g :: gs ->
+         match g () with
+         | None -> rgs := gs; go ()
+         | e -> e
+    in
+    go
 end

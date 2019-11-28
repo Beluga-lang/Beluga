d78af6d251828f37c393653300deee1922e1e06b
Jacob Errington <git@mail.jerrington.me>
2019-11-28 14:24:10 -0500

    delete the old lexer and parser

--- src/core/old-lexer.mli
+++
@@ -1,3 +0,0 @@
-(** Create a function mapping an initial location (needed for the file
-    name) and a stream of characters to a stream of located tokens. *)
-val mk : unit -> (Location.t -> char Stream.t -> (Token.t * Location.t) LinkStream.t)

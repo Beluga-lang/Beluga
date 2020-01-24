21643b64eaa38ff81cdfec09e33c8236347a4456
Jacob Errington <git@mail.jerrington.me>
2020-01-24 14:18:30 -0500

    support/fmt: add a type alias for formatters

--- src/support/fmt.ml
+++ src/support/fmt.ml
@@ -1,21 +1,27 @@
 open Format
 
+(** The type of formatting functions. *)
+type 'a t = formatter -> 'a -> unit
+
+(** Polymorphic function that processes a format string. *)
 type fmt =
   { fmt : 'a. ('a, formatter, unit) format -> 'a }
 
+(** Converts something to a string using a formatting function. *)
 let stringify p x = fprintf str_formatter "%a" p x; flush_str_formatter ()
+
+(** Prints a comma followed by a (breaking) space. *)
 let comma (ppf : Format.formatter) () = fprintf ppf ",@ "
 let between before after inside ppf () =
   before ppf ();
   inside ppf ();
   after ppf ()
 
-let surrounded
-      (by : Format.formatter -> unit -> unit)
-      (inside : Format.formatter -> 'a -> unit)
-      (ppf : Format.formatter)
-      (x : 'a)
-    : unit =
+(** `surrounded by inside` constructs a formatting function that
+    will print `by` then `inside` then `by` again.
+ *)
+let surrounded (by : unit t) (inside : 'a t) : 'a t =
+  fun ppf x ->
   by ppf ();
   inside ppf x;
   by ppf ()

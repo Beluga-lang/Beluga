e8c732d1740b7315ff34fa961a81cbaab02e876d
Jacob Errington <git@mail.jerrington.me>
2020-02-12 18:01:57 -0500

    erase: relax numeric_order to allow for Inductive PiBox

--- src/core/erase.ml
+++ src/core/erase.ml
@@ -8,7 +8,12 @@ let rec numeric_order_arg tau n =
   | TypPiBox (_, LF.Decl (_, _, d), tau), n ->
      let c = match d with
        | LF.Maybe -> 0
-       | LF.Inductive -> Error.violation "[erase] [numeric_order] Inductive is forbidden"
+       | LF.Inductive -> 1
+       (* We count Inductive as 1 instead of throwing an error because
+          we would elaboration only works when we don't have
+          Inductive. Inductive can however show up later, but we know
+          that it couldn't have been on an implicit argument (this is
+          forbidden). *)
        | LF.No -> 1
      in
      c + numeric_order_arg tau (n-1)

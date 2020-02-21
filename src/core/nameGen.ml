47f253e79b88baff274efeab6a564966a8bd11f2
Jacob Errington <git@mail.jerrington.me>
2020-02-21 09:48:24 -0500

    nameGen: add for comp variables; refactor; add mli, docs

--- src/core/nameGen.ml
+++ src/core/nameGen.ml
@@ -1,21 +1,47 @@
-(** Contextual name generator.
-    This name generator will use programmer-supplied naming hints
-    (falling back to good old X, Y, Z) but adjust the numeric counter
-    on the name so it is the smallest allowed in a given context.
- *)
+
 
 open Support
 
 module LF = Syntax.Int.LF
 module Comp = Syntax.Int.Comp
 
-(** Generate a raw metavariable name for a given LF type.
-    Uses programmer-supplied name hints with a fallback.
- *)
+let mvar_string tA = match Store.Cid.Typ.gen_mvar_name tA with
+  | None -> "X"
+  | Some g -> g ()
+
+let pvar_string tA = match Store.Cid.Typ.gen_var_name tA with
+  | None -> "p"
+  | Some g -> g ()
+
+let bvar_string tA = match Store.Cid.Typ.gen_var_name tA with
+  | None -> "x"
+  | Some g -> g ()
+
 let mvar (tA : LF.typ) : Id.name =
-  match Store.Cid.Typ.gen_mvar_name tA with
-  | None -> Id.(mk_name (SomeString "X"))
-  | Some g -> Id.(mk_name (SomeString (g ())))
+  Id.(mk_name (SomeString (mvar_string tA)))
+
+let pvar (tA : LF.typ) : Id.name =
+  Id.(mk_name (SomeString ("#" ^ pvar_string tA)))
+
+let bvar (tA : LF.typ) : Id.name =
+  Id.(mk_name (SomeString (bvar_string tA)))
+
+let rec var_string tau = match tau with
+  | Comp.TypBox (_, mT) ->
+     begin match mT with
+     | LF.CTyp _ ->
+        Error.violation
+          "[NameGen.var] computational CTyp impossible"
+     | LF.ClTyp (cU, _) ->
+        match cU with
+        | LF.(MTyp tA | PTyp tA) -> bvar_string tA
+        | LF.STyp _ -> "s"
+     end
+  | Comp.(TypPiBox (_, _, tau) | TypArr (_, _, tau) | TypClo (tau, _)) ->
+     var_string tau
+  | _ -> "x"
+
+let var tau = Id.(mk_name (SomeString (var_string tau)))
 
 let renumber (ctx : Id.name list) name =
   match Id.max_usage ctx (Id.base_name name) with
---
+++ src/core/nameGen.mli
@@ -0,0 +1,52 @@
+(** Contextual type-based name generator.
+    This name generator will use programmer-supplied naming hints
+    (falling back to good old X, Y, Z) but adjust the numeric counter
+    on the name so it is the smallest allowed in a given context.
+
+    This name generation strategy notably does not use any global
+    counters, so its results are independent of order of operations.
+    The result of this generator depends only on what ambient context
+    of names is passed as input.
+
+    The general idiom to use this generator is:
+        NameGen.(var tau |> renumber names)
+    where
+        names : Id.name list
+    can be calculated using helper functions from context.ml.
+    (See the names_of_Xctx family of functions.)
+
+    Caveats.
+    1. In general you need to be conscious of when you calculate
+       name lists, as this does incur a traversal of the context.
+    2. This name generation strategy is strictly less
+       efficient than global variable-based strategies, as the
+       `renumber` function -- the core algorithm of this module --
+       traverses the entire name list to decide what number to assign
+       the input name.
+
+    As for point 2, a more efficient data structure for representing
+    the name list could improve efficiency substantially. In
+    particular, a hashtable or a trie that associates to each name its
+    highest count.
+ *)
+
+open Syntax.Int
+
+(** Generates a raw parameter variable name for a given LF type.
+    Uses the programmer-supplied name hint, with a fallback.
+ *)
+val pvar : LF.typ -> Id.name
+
+(** Generates a raw bound variable name for a given LF type.
+    Uses the programmer-supplied name hint, with a fallback.
+ *)
+val bvar : LF.typ -> Id.name
+
+(** Generate a raw metavariable name for a given LF type.
+    Uses programmer-supplied name hints with a fallback.
+ *)
+val mvar : LF.typ -> Id.name
+
+val var : Comp.typ -> Id.name
+
+val renumber : Id.name list -> Id.name -> Id.name

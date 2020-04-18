72097c339688c34105b93266181b011aba7281c6
Junyoung Clare Jang <jjc9310@gmail.com>
2020-04-18 03:43:38 -0400

    chatter: make a small code style change

--- src/core/chatter.ml
+++ src/core/chatter.ml
@@ -3,9 +3,9 @@ let level : int ref = ref 1
 let ppf = Format.std_formatter
 
 let print lvl x =
-  Format.fprintf
-    (if lvl <= !level then
-       ppf
-     else
-       Support.Fmt.null_formatter)
-    x
+  let ppf =
+    if lvl <= !level
+    then ppf
+    else Support.Fmt.null_formatter
+  in
+  Format.fprintf ppf x

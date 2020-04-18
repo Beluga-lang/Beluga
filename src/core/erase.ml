ec741fc96a470cc7c7a356eb86162b8fef460502
Junyoung Clare Jang <jjc9310@gmail.com>
2020-04-18 02:52:14 -0400

    erase: style the module code

--- src/core/erase.ml
+++ src/core/erase.ml
@@ -2,11 +2,12 @@ open Syntax.Int
 
 let rec numeric_order_arg tau n =
   let open Comp in
-  match tau, n with
-  | _, 0 -> 0
-  | TypArr (_, _, tau), n -> 1 + numeric_order_arg tau (n-1)
-  | TypPiBox (_, LF.Decl (_, _, d), tau), n ->
-     let c = match d with
+  match (tau, n) with
+  | (_, 0) -> 0
+  | (TypArr (_, _, tau), n) -> 1 + numeric_order_arg tau (n-1)
+  | (TypPiBox (_, LF.Decl (_, _, d), tau), n) ->
+     let c =
+       match d with
        | LF.Maybe -> 0
        | LF.Inductive -> 1
        (* We count Inductive as 1 instead of throwing an error because

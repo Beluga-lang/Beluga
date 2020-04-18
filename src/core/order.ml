f3149fd3c8346aee8ad6d847f96497cfbe4cb90b
Junyoung Clare Jang <jjc9310@gmail.com>
2020-04-17 21:43:12 -0400

    order: style the module code

--- src/core/order.ml
+++ src/core/order.ml
@@ -3,8 +3,8 @@ open Support
 module Comp = Syntax.Ext.Comp
 module I = Syntax.Int.Comp
 
-let rec of_numeric_order (o : Comp.numeric_order) : I.order =
-  match o with
+let rec of_numeric_order : Comp.numeric_order -> I.order =
+  function
   | Comp.Arg n -> I.Arg n
   | Comp.Lex xs -> I.Lex (List.map of_numeric_order xs)
   | Comp.Simul xs -> I.Simul (List.map of_numeric_order xs)
@@ -18,21 +18,23 @@ let rec of_numeric_order (o : Comp.numeric_order) : I.order =
 (*   ih a(i+1) .. an as long as the arguments are smaller or equal  *)
 (* then the ones of ai.                                             *)
 
-type mutual =		                (* Mutual dependencies        *)
-    Empty				(* C ::= .                    *)
-  | LE of Id.cid_prog * mutual	(*     |  <= (a) C            *)
-  | LT of Id.cid_prog * mutual	(*     |  > (a) C             *)
+type mutual =                   (* Mutual dependencies        *)
+  | Empty                       (* C ::= .                    *)
+  | LE of Id.cid_prog * mutual  (*     |  <= (a) C            *)
+  | LT of Id.cid_prog * mutual  (*     |  > (a) C             *)
 
-type dec =                           (* Termination declaration    *)
-    Dec of I.order * mutual            (* Dec ::= (O, C)            *)
+type dec =                             (* Termination declaration    *)
+  | Dec of I.order * mutual            (* Dec ::= (O, C)            *)
 
 (** Converts the order to a list of argument positions
     If the order is too complicated, returns None.
  *)
-let list_of_order : I.order -> int list option = function
+let list_of_order : I.order -> int list option =
+  function
   | I.Arg x -> Some [x]
   | I.Lex xs ->
-     let f = function
+     let f =
+       function
        | I.Arg x -> Some x
        | _ -> None (* We don't support nested lexicographic orders. *)
      in

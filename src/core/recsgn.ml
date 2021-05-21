f1533f22a53dc568d24cfe8b0810d46bcb121051
yasharthb <yasharthbajpai.99@gmail.com>
2021-05-21 21:38:22 +0530

    Added depth option to MQuery 	Made relevant changes through the parser into logic. 	Added Exceptions to be triggered when Depth is hit 	Altered pretty printing to accomodate the depth option 	Modifed Example Queries accordingly

--- src/core/recsgn.ml
+++ src/core/recsgn.ml
@@ -1146,7 +1146,7 @@ let recSgnDecls decls =
        Int.Sgn.Query (loc, name, cD, (tA', i), expected, tries)
 
 
-    | Ext.Sgn.MQuery (loc, tau, expected, tries) ->
+    | Ext.Sgn.MQuery (loc, tau, expected, tries, depth) ->
        (dprintf
          (fun p ->
            p.fmt "[RecSgn Checking] MQuery at %a"
@@ -1183,8 +1183,8 @@ let recSgnDecls decls =
           , fun () ->
            Check.Comp.checkTyp Int.LF.Empty tau'
           );
-        Logic.storeMQuery (tau', i) expected tries)   ;                 
-        Int.Sgn.MQuery (loc, (tau', i), expected, tries)))
+        Logic.storeMQuery (tau', i) expected tries depth)   ;                 
+        Int.Sgn.MQuery (loc, (tau', i), expected, tries, depth)))
 
 
        

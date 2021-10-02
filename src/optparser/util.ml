fc2e988a1e51e998d42ac28dfd69d4a536fe292c
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2021-10-02 16:01:28 -0400

    Remove functions and modules provided by newer `Stdlib`

--- src/optparser/util.ml
+++ src/optparser/util.ml
@@ -8,16 +8,6 @@ let id a = a
 module List = struct
   include Stdlib.List
 
-  let filter_map f l =
-    let rec loop = function
-      | [] -> []
-      | h :: t ->
-        match f h with
-        | None -> loop t
-        | Some x -> x :: loop t
-    in
-    loop l
-
   let take_circularly n ls =
     let rec go n =
       function
@@ -30,32 +20,4 @@ module List = struct
     if ls = []
     then raise (Invalid_argument "'ls' should have more than one element")
     else go n ls
-end
-
-module Option = struct
-  let some a = Some a
-
-  let fold ~none ~some =
-    function
-    | None -> none
-    | Some a -> some a
-
-  let map f = fold ~none:None ~some:(fun a -> Some (f a))
-
-  let to_list (a : 'a option) : 'a list =
-    fold ~none:[] ~some:(fun a -> [a]) a
-  let to_seq (a : 'a option) : 'a Seq.t =
-    fold ~none:Seq.empty ~some:Seq.return a
-end
-
-module Result = struct
-  let ok a = Ok a
-  let error e = Error e
-
-  let fold ~error ~ok =
-    function
-    | Error e -> error e
-    | Ok a -> ok a
-
-  let map f = fold ~error ~ok:(fun a -> Ok (f a))
 end

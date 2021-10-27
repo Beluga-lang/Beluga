566fdcc1304c87105e423cfe481d473b8740a3dd
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2021-10-27 17:51:25 -0400

    Add documentation to `OptParser.List.take_circularly`

---
+++ src/optparser/list.mli
@@ -0,0 +1,8 @@
+include module type of Stdlib.List
+
+(** [take_circularly k [x1; x2; ...; xn]] is the list of the first [k] elements
+    taken fron the sequence [[x1; x2; ...; xn; x1; x2; ...; xn; x1; ...]].
+    If [k < 0], then [[]] is returned.
+    @raise Invalid_argument if the input list is empty.
+*)
+val take_circularly : int -> 'a t -> 'a t

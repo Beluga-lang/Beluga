37ad92f59d4da1f0515802bad7df2eead237d282
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2022-02-19 17:19:52 -0500

    Use transforms for option attributes

--- src/optparser/option.ml
+++
@@ -1,3 +0,0 @@
-include Stdlib.Option
-
-let ( <|> ) o1 o2 = fold ~none:o2 ~some o1
--- src/optparser/option.mli
+++
@@ -1,4 +0,0 @@
-include module type of Stdlib.Option
-
-(** [o1 <|> o2] is [o1] if [o1] is [Some v], and [o2] otherwise. *)
-val ( <|> ) : 'a option -> 'a option -> 'a option

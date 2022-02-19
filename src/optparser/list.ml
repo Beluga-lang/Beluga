5718f03becfb4a2105a95c1b56cba6ddae0ada34
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2022-02-19 10:30:00 -0500

    Extract `List.split` function from parser

--- src/optparser/list.ml
+++ src/optparser/list.ml
@@ -16,3 +16,14 @@ let take_circularly n ls =
 
 let maximum_element : type a. (a -> a -> a) -> a -> a list -> a =
  fun maximum default -> fold_left maximum default
+
+
+let split n =
+  let rec loop n = function
+    | x :: xs when n > 0 ->
+        let xs', ys = loop (n - 1) xs in
+        (x :: xs', ys)
+    | xs ->
+        ([], xs)
+  in
+  loop n

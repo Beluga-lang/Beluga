28cfefd468bd1177ad674e3bb9509f398bd29136
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2022-06-01 13:02:44 -0400

    Remove duplicate type annotations

--- src/support/pureStack.ml
+++ src/support/pureStack.ml
@@ -1,32 +1,26 @@
 type 'a t =
-  { get : 'a list;
-    len : int;
+  { get : 'a list
+  ; len : int
   }
 
-let empty : 'a t =
-  { get = []; len = 0 }
+let empty = { get = []; len = 0 }
 
-let length (s : 'a t) : int =
-  s.len
+let length s = s.len
 
-let to_list (s : 'a t) : 'a list =
-  s.get
+let to_list s = s.get
 
-let is_empty (s : 'a t) : bool =
-  s.len = 0
+let is_empty s = s.len = 0
 
-let push (x : 'a) (s : 'a t) =
-  { get = x :: s.get; len = s.len + 1 }
+let push x s = { get = x :: s.get; len = s.len + 1 }
 
-let pop (s : 'a t) : ('a * 'a t) option =
+let pop s =
   match s.get with
-  | [] -> None
-  | (x :: xs) -> Some (x, { get = xs; len = s.len - 1 })
+  | [] -> Option.none
+  | x :: xs -> Option.some (x, { get = xs; len = s.len - 1 })
 
-let cut (n : int) (s : 'a t) : ('a list * 'a t) option =
-  let rec go (n : int) (s : 'a t) (acc : 'a list) : ('a list * 'a t) option =
-    if n <= 0 then
-      Some (acc, s)
+let cut n s =
+  let rec go n s acc =
+    if n <= 0 then Some (acc, s)
     else
       match pop s with
       | None -> None

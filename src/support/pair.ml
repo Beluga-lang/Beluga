5862ae2c3e129bb3657e3ad623a974b283ec88a8
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2022-05-31 22:02:36 -0400

    Remove duplicate type annotations for `Support.Pair`
    The type annotations are found in the corresponding `.mli` file.

--- src/support/pair.ml
+++ src/support/pair.ml
@@ -1,23 +1,18 @@
 type ('a, 'b) t = 'a * 'b
 
-let rmap (f : 'a -> 'b) (p : 'x * 'a) : 'x * 'b =
-  let (x, a) = p in (x, f a)
+let rmap f (x, a) = (x, f a)
 
-let lmap (f : 'a -> 'b) (p : 'a * 'x) : 'b * 'x =
-  let (a, x) = p in (f a, x)
+let lmap f (a, x) = (f a, x)
 
-let bimap (f : 'a -> 'b) (g : 'c -> 'd) (p : 'a * 'c) =
-  let (x, y) = p in (f x, g y)
+let bimap f g (x, y) = (f x, g y)
 
-let both (f : 'a -> 'b) (p : 'a * 'a) : 'b * 'b =
-  bimap f f p
+let both f p = bimap f f p
 
-let left (x : 'a) (y : 'b) : 'a * 'b = (x, y)
+let left x y = (x, y)
 
-let right (x : 'a) (y : 'b) : 'b * 'a = (y, x)
+let right x y = (y, x)
 
-let swap (p : 'a * 'b) : 'b * 'a =
-  let (x, y) = p in (y, x)
+let swap (x, y) = (y, x)
 
 let curry f x y = f (x, y)
 

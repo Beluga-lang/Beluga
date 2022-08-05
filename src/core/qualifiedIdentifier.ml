656409ebb8e545723361890279fd5f4b76ad7245
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2022-08-05 17:04:38 -0400

    Add `QualifiedIdentifier.make_simple`

--- src/core/qualifiedIdentifier.ml
+++ src/core/qualifiedIdentifier.ml
@@ -8,6 +8,10 @@ type t =
 
 let make ~location ?(modules = []) name = { location; name; modules }
 
+let make_simple name =
+  let location = Identifier.location name in
+  make ~location name
+
 let[@inline] location { location; _ } = location
 
 let[@inline] name { name; _ } = name

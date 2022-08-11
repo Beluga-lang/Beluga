9df04722d954c6e956bd39cfcea70647415e22b0
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2022-08-11 18:00:43 -0400

    Test `Dictionary` module

--- test/beluga/test_beluga.ml
+++ test/beluga/test_beluga.ml
@@ -2,4 +2,8 @@ open OUnit2
 
 let () =
   run_test_tt_main
-    ("Beluga" >::: [ Test_ShuntingYard.tests; Test_LF_parser.tests ])
+    ("Beluga"
+    >::: [ Test_ShuntingYard.tests
+         ; Test_parser_dictionary.tests
+         ; Test_LF_parser.tests
+         ])

046b28b3f4a1313f6024effb46eb4a84dc9d3def
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2022-08-11 17:59:40 -0400

    Revise code style for first-class modules

--- src/support/hash.ml
+++ src/support/hash.ml
@@ -12,13 +12,10 @@ end) : HASH with type t = T.t = struct
   let hash = Hashtbl.hash
 end
 
-let contramap (type t s) (hash : (module HASH with type t = s))
-    (f : t -> s) =
+let contramap (type t s) (module Hash : HASH with type t = s) (f : t -> s) =
   (module struct
     type nonrec t = t
 
-    let hash =
-      let (module Hash) = hash in
-      fun x -> Hash.hash (f x)
+    let hash x = Hash.hash (f x)
   end : HASH
     with type t = t)

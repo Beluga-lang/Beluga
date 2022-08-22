0908e3b9b5780b4f965816979c7d92610b76663b
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2022-08-22 16:25:35 -0400

    Implement elaboration to external contextual LF types and terms
    It remains to define type-level and term-level contextual LF patterns and their elaboration.
    The elaboration for types and terms may not be reused since holes may appear at the type level.

--- src/core/prettyext'.ml
+++ src/core/prettyext'.ml
@@ -246,3 +246,247 @@ module LF = struct
         Format.fprintf ppf "@[<2>TermParenthesized(%a)@]" pp_term term
   end
 end
+
+module CLF = struct
+  open CLF
+
+  let rec pp_typ ppf typ =
+    match typ with
+    | Typ.Constant { identifier; _ } ->
+      Format.fprintf ppf "%a" QualifiedIdentifier.pp identifier
+    | Typ.Application { applicand; arguments = []; _ } ->
+      Format.fprintf ppf "@[<2>%a@]" pp_typ applicand
+    | Typ.Application { applicand; arguments; _ } ->
+      Format.fprintf ppf "@[<2>%a@ %a@]" pp_typ applicand
+        (List.pp ~pp_sep:(fun ppf () -> Format.fprintf ppf "@ ") pp_term)
+        arguments
+    | Typ.ForwardArrow { domain; range; _ } ->
+      Format.fprintf ppf "@[<2>%a@ ->@ %a@]" pp_typ domain pp_typ range
+    | Typ.BackwardArrow { domain; range; _ } ->
+      Format.fprintf ppf "@[<2>%a@ <-@ %a@]" pp_typ domain pp_typ range
+    | Typ.Pi { parameter_identifier = Option.None; parameter_type; body; _ }
+      ->
+      Format.fprintf ppf "@[<2>{@ _@ :@ %a@ }@ %a@]" pp_typ parameter_type
+        pp_typ body
+    | Typ.Pi
+        { parameter_identifier = Option.Some parameter_identifier
+        ; parameter_type
+        ; body
+        ; _
+        } ->
+      Format.fprintf ppf "@[<2>{@ %a@ :@ %a@ }@ %a@]" Identifier.pp
+        parameter_identifier pp_typ parameter_type pp_typ body
+    | Typ.Block { elements = (Option.None, typ), []; _ } ->
+      Format.fprintf ppf "@[<2>block %a]" pp_typ typ
+    | Typ.Block { elements = (Option.None, _typ), _nt1 :: _nts; _ } ->
+      raise
+      @@ Invalid_argument
+           "[pp_typ] missing identifier for first type in block"
+    | Typ.Block { elements = (Option.Some i1, t1), nts; _ } ->
+      Format.fprintf ppf "@[<2>block (%a)]"
+        (List.pp
+           ~pp_sep:(fun ppf () -> Format.fprintf ppf ",@,")
+           (fun ppf (i, t) ->
+             Format.fprintf ppf "%a@ :@ %a" Identifier.pp i pp_typ t))
+        ((i1, t1) :: nts)
+    | Typ.Parenthesized { typ; _ } ->
+      Format.fprintf ppf "@[<2>(%a)@]" pp_typ typ
+
+  and pp_term ppf term =
+    match term with
+    | Term.Variable { identifier; _ } ->
+      Format.fprintf ppf "%a" Identifier.pp identifier
+    | Term.Constant { identifier; _ } ->
+      Format.fprintf ppf "%a" QualifiedIdentifier.pp identifier
+    | Term.Application { applicand; arguments = []; _ } ->
+      Format.fprintf ppf "@[<2>%a@]" pp_term applicand
+    | Term.Application { applicand; arguments; _ } ->
+      Format.fprintf ppf "@[<2>%a@ %a@]" pp_term applicand
+        (List.pp ~pp_sep:(fun ppf () -> Format.fprintf ppf "@ ") pp_term)
+        arguments
+    | Term.Abstraction
+        { parameter_identifier = Option.None
+        ; parameter_type = Option.None
+        ; body
+        ; _
+        } -> Format.fprintf ppf "@[<2>\\_.@ %a@]" pp_term body
+    | Term.Abstraction
+        { parameter_identifier = Option.None
+        ; parameter_type = Option.Some parameter_type
+        ; body
+        ; _
+        } ->
+      Format.fprintf ppf "@[<2>\\_:%a.@ %a@]" pp_typ parameter_type pp_term
+        body
+    | Term.Abstraction
+        { parameter_identifier = Option.Some parameter_identifier
+        ; parameter_type = Option.None
+        ; body
+        ; _
+        } ->
+      Format.fprintf ppf "@[<2>\\%a.@ %a@]" Identifier.pp
+        parameter_identifier pp_term body
+    | Term.Abstraction
+        { parameter_identifier = Option.Some parameter_identifier
+        ; parameter_type = Option.Some parameter_type
+        ; body
+        ; _
+        } ->
+      Format.fprintf ppf "@[<2>\\%a:%a.@ %a@]" Identifier.pp
+        parameter_identifier pp_typ parameter_type pp_term body
+    | Term.Hole _ -> Format.fprintf ppf "_"
+    | Term.Substitution { term; substitution; _ } ->
+      Format.fprintf ppf "@[<2>%a%a@]" pp_term term pp_substitution
+        substitution
+    | Term.Tuple { terms; _ } ->
+      Format.fprintf ppf "@[<2><%a>@]"
+        (List1.pp ~pp_sep:(fun ppf () -> Format.fprintf ppf ";@,") pp_term)
+        terms
+    | Term.Projection { term; projection = `By_position i; _ } ->
+      Format.fprintf ppf "@[<2>%a.%d@]" pp_term term i
+    | Term.Projection { term; projection = `By_identifier i; _ } ->
+      Format.fprintf ppf "@[<2>%a.%a@]" pp_term term Identifier.pp i
+    | Term.TypeAnnotated { term; typ; _ } ->
+      Format.fprintf ppf "@[<2>%a@ :@ %a@]" pp_term term pp_typ typ
+    | Term.Parenthesized { term; _ } ->
+      Format.fprintf ppf "@[<2>(%a)@]" pp_term term
+
+  and pp_substitution ppf substitution =
+    match substitution with
+    | Substitution.{ extends_identity = false; terms = []; _ } ->
+      Format.fprintf ppf "[]"
+    | Substitution.{ extends_identity = true; terms = []; _ } ->
+      Format.fprintf ppf "[..]"
+    | Substitution.{ extends_identity = false; terms; _ } ->
+      Format.fprintf ppf "@[<2>[%a]@]"
+        (List.pp ~pp_sep:(fun ppf () -> Format.fprintf ppf ",@,") pp_term)
+        terms
+    | Substitution.{ extends_identity = true; terms; _ } ->
+      Format.fprintf ppf "@[<2>[..,@,%a]@]"
+        (List.pp ~pp_sep:(fun ppf () -> Format.fprintf ppf ",@,") pp_term)
+        terms
+
+  module Debug = struct
+    let rec pp_typ ppf typ =
+      match typ with
+      | Typ.Constant { identifier; _ } ->
+        Format.fprintf ppf "%a" QualifiedIdentifier.pp identifier
+      | Typ.Application { applicand; arguments = []; _ } ->
+        Format.fprintf ppf "@[<2>TypeApplication(%a)@]" pp_typ applicand
+      | Typ.Application { applicand; arguments; _ } ->
+        Format.fprintf ppf "@[<2>TypeApplication(%a@ %a)@]" pp_typ applicand
+          (List.pp ~pp_sep:(fun ppf () -> Format.fprintf ppf "@ ") pp_term)
+          arguments
+      | Typ.ForwardArrow { domain; range; _ } ->
+        Format.fprintf ppf "@[<2>TypeArrow(%a@ ->@ %a)@]" pp_typ domain
+          pp_typ range
+      | Typ.BackwardArrow { domain; range; _ } ->
+        Format.fprintf ppf "@[<2>TypeArrow(%a@ <-@ %a)@]" pp_typ domain
+          pp_typ range
+      | Typ.Pi
+          { parameter_identifier = Option.None; parameter_type; body; _ } ->
+        Format.fprintf ppf "@[<2>TypePi({@ _@ :@ %a@ }@ %a)@]" pp_typ
+          parameter_type pp_typ body
+      | Typ.Pi
+          { parameter_identifier = Option.Some parameter_identifier
+          ; parameter_type
+          ; body
+          ; _
+          } ->
+        Format.fprintf ppf "@[<2>TypePi({@ %a@ :@ %a@ }@ %a)@]" Identifier.pp
+          parameter_identifier pp_typ parameter_type pp_typ body
+      | Typ.Block { elements = (Option.None, typ), []; _ } ->
+        Format.fprintf ppf "@[<2>TypeBlock(block %a)]" pp_typ typ
+      | Typ.Block { elements = (Option.None, _typ), _nt1 :: _nts; _ } ->
+        raise
+        @@ Invalid_argument
+             "[pp_typ] missing identifier for first type in block"
+      | Typ.Block { elements = (Option.Some i1, t1), nts; _ } ->
+        Format.fprintf ppf "@[<2>TypeBlock(block (%a))]"
+          (List.pp
+             ~pp_sep:(fun ppf () -> Format.fprintf ppf ",@,")
+             (fun ppf (i, t) ->
+               Format.fprintf ppf "%a@ :@ %a" Identifier.pp i pp_typ t))
+          ((i1, t1) :: nts)
+      | Typ.Parenthesized { typ; _ } ->
+        Format.fprintf ppf "@[<2>TypeParenthesized(%a)@]" pp_typ typ
+
+    and pp_term ppf term =
+      match term with
+      | Term.Variable { identifier; _ } ->
+        Format.fprintf ppf "%a" Identifier.pp identifier
+      | Term.Constant { identifier; _ } ->
+        Format.fprintf ppf "%a" QualifiedIdentifier.pp identifier
+      | Term.Application { applicand; arguments = []; _ } ->
+        Format.fprintf ppf "@[<2>TermApplication(%a)@]" pp_term applicand
+      | Term.Application { applicand; arguments; _ } ->
+        Format.fprintf ppf "@[<2>TermApplication(%a@ %a)@]" pp_term applicand
+          (List.pp ~pp_sep:(fun ppf () -> Format.fprintf ppf "@ ") pp_term)
+          arguments
+      | Term.Abstraction
+          { parameter_identifier = Option.None
+          ; parameter_type = Option.None
+          ; body
+          ; _
+          } ->
+        Format.fprintf ppf "@[<2>TermAbstraction(\\_.@ %a)@]" pp_term body
+      | Term.Abstraction
+          { parameter_identifier = Option.None
+          ; parameter_type = Option.Some parameter_type
+          ; body
+          ; _
+          } ->
+        Format.fprintf ppf "@[<2>TermAbstraction(\\_:%a.@ %a)@]" pp_typ
+          parameter_type pp_term body
+      | Term.Abstraction
+          { parameter_identifier = Option.Some parameter_identifier
+          ; parameter_type = Option.None
+          ; body
+          ; _
+          } ->
+        Format.fprintf ppf "@[<2>TermAbstraction(\\%a.@ %a)@]" Identifier.pp
+          parameter_identifier pp_term body
+      | Term.Abstraction
+          { parameter_identifier = Option.Some parameter_identifier
+          ; parameter_type = Option.Some parameter_type
+          ; body
+          ; _
+          } ->
+        Format.fprintf ppf "@[<2>TermAbstraction(\\%a:%a.@ %a)@]"
+          Identifier.pp parameter_identifier pp_typ parameter_type pp_term
+          body
+      | Term.Hole _ -> Format.fprintf ppf "_"
+      | Term.Substitution { term; substitution; _ } ->
+        Format.fprintf ppf "@[<2>TermSubstitution(%a%a)@]" pp_term term
+          pp_substitution substitution
+      | Term.Tuple { terms; _ } ->
+        Format.fprintf ppf "@[<2>TermTuple(<%a>)@]"
+          (List1.pp ~pp_sep:(fun ppf () -> Format.fprintf ppf ";@,") pp_term)
+          terms
+      | Term.Projection { term; projection = `By_position i; _ } ->
+        Format.fprintf ppf "@[<2>TermProjection(%a.%d)@]" pp_term term i
+      | Term.Projection { term; projection = `By_identifier i; _ } ->
+        Format.fprintf ppf "@[<2>TermProjection(%a.%a)@]" pp_term term
+          Identifier.pp i
+      | Term.TypeAnnotated { term; typ; _ } ->
+        Format.fprintf ppf "@[<2>TermAnnotated(%a@ :@ %a)@]" pp_term term
+          pp_typ typ
+      | Term.Parenthesized { term; _ } ->
+        Format.fprintf ppf "@[<2>TermParenthesized(%a)@]" pp_term term
+
+    and pp_substitution ppf substitution =
+      match substitution with
+      | Substitution.{ extends_identity = false; terms = []; _ } ->
+        Format.fprintf ppf "Substitution([])"
+      | Substitution.{ extends_identity = true; terms = []; _ } ->
+        Format.fprintf ppf "Substitution([..])"
+      | Substitution.{ extends_identity = false; terms; _ } ->
+        Format.fprintf ppf "@[<2>Substitution([%a])@]"
+          (List.pp ~pp_sep:(fun ppf () -> Format.fprintf ppf ",@,") pp_term)
+          terms
+      | Substitution.{ extends_identity = true; terms; _ } ->
+        Format.fprintf ppf "@[<2>Substitution([..,@,%a])@]"
+          (List.pp ~pp_sep:(fun ppf () -> Format.fprintf ppf ",@,") pp_term)
+          terms
+  end
+end
--- src/core/synext'.ml
+++ src/core/synext'.ml
@@ -3,8 +3,8 @@ open Support
 (** {1 External LF Syntax}
 
     The representation of LF kinds, types, and terms after parsing and
-    data-dependent disambiguation. ASTs constructed with the constructors
-    therein are not necessarily normalized.
+    data-dependent disambiguation. ASTs constructed with the constructors in
+    this module are not necessarily normalized.
 
     These types are only intended to be used in the definition of LF
     type-level or term-level constants. This is a weak, representational
@@ -159,4 +159,192 @@ module LF = struct
     | Term.Wildcard { location; _ }
     | Term.TypeAnnotated { location; _ }
     | Term.Parenthesized { location; _ } -> location
+end
+
+(** {1 External Contextual LF Syntax}
+
+    The representation of contextual LF types, terms, and patterns after
+    parsing and data-dependent disambiguation. ASTs constructed with the
+    constructors in this module are not necessarily normalized.
+
+    The distinction between contextual LF objects and plain LF objects is
+    that contextual LF objects may have substitutions, and may appear in
+    patterns.
+
+    These types are suited for pretty-printing and elaboration to the
+    internal syntax. Note that this is a named representation. *)
+module CLF = struct
+  (** External contextual LF types. *)
+  module rec Typ : sig
+    type t =
+      | Constant of
+          { location : Location.t
+          ; identifier : QualifiedIdentifier.t
+          ; operator : Operator.t
+          }
+          (** [Constant { identifier; _ }] is the type-level constant with
+              qualified identifier `identifier', which is necessarily bound. *)
+      | Application of
+          { location : Location.t
+          ; applicand : Typ.t
+          ; arguments : Term.t List.t
+          }
+          (** [Application { applicand; arguments }] is the type-level
+              application of `applicand' with arguments `arguments'. *)
+      | ForwardArrow of
+          { location : Location.t
+          ; domain : Typ.t
+          ; range : Typ.t
+          }
+          (** [ForwardArrow { domain; range; _ }] is the type `domain ->
+              range'. *)
+      | BackwardArrow of
+          { location : Location.t
+          ; domain : Typ.t
+          ; range : Typ.t
+          }
+          (** [BackwardArrow { domain; range; _ }] is the type `domain <-
+              range'. *)
+      | Pi of
+          { location : Location.t
+          ; parameter_identifier : Identifier.t Option.t
+          ; parameter_type : Typ.t
+          ; body : Typ.t
+          }
+          (** [Pi { parameter_identifier = x; parameter_type = t; body; _ }]
+              is the dependent product type `[{ x : t } body]'. *)
+      | Block of
+          { location : Location.t
+          ; elements :
+              (Identifier.t Option.t * Typ.t) * (Identifier.t * Typ.t) List.t
+          }
+          (** [Block { elements; _ }] is the block type `block (elements)'.
+
+              - If [elements = ((Option.None, _typ), rest)], then
+                [rest = \[\]].
+              - If [elements = ((label, _typ), rest)] with [rest <> \[\]],
+                then [label = Option.Some identifier]. *)
+      | Parenthesized of
+          { location : Location.t
+          ; typ : Typ.t
+          }  (** [Parenthesized { typ; _ }] is the type `( typ )`. *)
+  end =
+    Typ
+
+  (** External contextual LF terms. *)
+  and Term : sig
+    type t =
+      | Variable of
+          { location : Location.t
+          ; identifier : Identifier.t
+          }
+          (** [Variable { identifier; _ }] is the term-level variable with
+              name `identifier', which is either a bound variable having a
+              lambda binder, or a free variable having no such corresponding
+              binder. *)
+      | Constant of
+          { location : Location.t
+          ; identifier : QualifiedIdentifier.t
+          ; operator : Operator.t
+          }
+          (** [Constant { identifier; _ }] is the term-level constant with
+              qualified identifier `identifier', which is necessarily bound. *)
+      | Substitution of
+          { location : Location.t
+          ; term : Term.t
+          ; substitution : Substitution.t
+          }
+          (** [Substitution { term; substitution; _ }] is the term
+              `term[substitution]'. *)
+      | Application of
+          { location : Location.t
+          ; applicand : Term.t
+          ; arguments : Term.t List.t
+          }
+          (** [Application { applicand; arguments }] is the term-level
+              application of `applicand' with arguments `arguments'. *)
+      | Abstraction of
+          { location : Location.t
+          ; parameter_identifier : Identifier.t Option.t
+          ; parameter_type : Typ.t Option.t
+          ; body : Term.t
+          }
+          (** [Abstraction { parameter_identifier = x; body; _ }] is the term
+              `\x. body'. *)
+      | Hole of { location : Location.t }
+          (** [Hole { _ }] is the omission of a term for reconstruction. *)
+      | Tuple of
+          { location : Location.t
+          ; terms : Term.t List1.t
+          }
+          (** [Tuple { terms; _ }] is the tuple term `<t1; t2; t3>' if
+              [List1.to_list terms = \[t1; t2; t3\]].
+
+              This should not be confused with computational-level tuples.
+              The type of a contextual LF term-level tuple is a block. *)
+      | Projection of
+          { location : Location.t
+          ; term : Term.t
+          ; projection :
+              [ `By_identifier of Identifier.t | `By_position of Int.t ]
+          }
+      | TypeAnnotated of
+          { location : Location.t
+          ; term : Term.t
+          ; typ : Typ.t
+          }
+          (** [TypeAnnotated { term = u; typ = t; _ }] is the term `u : t`. *)
+      | Parenthesized of
+          { location : Location.t
+          ; term : Term.t
+          }  (** [Parenthesized { term; _ }] is the term `( term )`. *)
+  end =
+    Term
+
+  and Substitution : sig
+    (** [{ extends_identity; terms; _ }] is either:
+
+        - The empty substitution `[]' if [extends_identity = false] and
+          [terms = \[\]]
+        - The identity substitution `[..]' if [extends_identity = true] and
+          [terms = \[\]]
+        - A plain substitution `[t1, t2, t3]' if [extends_identity = false]
+          and [terms = \[t1; t2; t3\]]
+        - A substitution that extends the identity substitution
+          `[.., t1, t2, t3]' if [extends_identity = true] and
+          [terms = \[t1; t2; t3\]] *)
+    type t =
+      { location : Location.t
+      ; extends_identity : Bool.t
+      ; terms : Term.t List.t
+      }
+  end =
+    Substitution
+
+  let location_of_typ typ =
+    match typ with
+    | Typ.Constant { location; _ }
+    | Typ.Application { location; _ }
+    | Typ.ForwardArrow { location; _ }
+    | Typ.BackwardArrow { location; _ }
+    | Typ.Pi { location; _ }
+    | Typ.Block { location; _ }
+    | Typ.Parenthesized { location; _ } -> location
+
+  let location_of_term term =
+    match term with
+    | Term.Variable { location; _ }
+    | Term.Constant { location; _ }
+    | Term.Application { location; _ }
+    | Term.Abstraction { location; _ }
+    | Term.Hole { location; _ }
+    | Term.Substitution { location; _ }
+    | Term.Tuple { location; _ }
+    | Term.Projection { location; _ }
+    | Term.TypeAnnotated { location; _ }
+    | Term.Parenthesized { location; _ } -> location
+
+  let location_of_substitution substitution =
+    match substitution with
+    | Substitution.{ location; _ } -> location
 end

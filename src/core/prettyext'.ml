453867a6231221298429eeedd60e3b9db9d4d8bb
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2022-08-22 23:30:11 -0400

    Implement functions to minimally parenthesize LF kinds, types and terms
    This will be used when generating string representations of programs synthesized from the internal syntax.

--- src/core/prettyext'.ml
+++ src/core/prettyext'.ml
@@ -4,6 +4,267 @@ open Synext'
 module LF = struct
   open LF
 
+  (** [remove_parentheses_kind kind] is [kind] without parentheses, except
+      for quoted type-level and term-level constants. Printing this kind may
+      not result in a syntactically valid LF kind. *)
+  let rec remove_parentheses_kind kind =
+    match kind with
+    | Kind.Typ _ -> kind
+    | Kind.Arrow { location; domain; range } ->
+      let domain' = remove_parentheses_typ domain
+      and range' = remove_parentheses_kind range in
+      Kind.Arrow { location; domain = domain'; range = range' }
+    | Kind.Pi { location; parameter_identifier; parameter_type; body } ->
+      let parameter_type' = remove_parentheses_typ parameter_type
+      and body' = remove_parentheses_kind body in
+      Kind.Pi
+        { location
+        ; parameter_identifier
+        ; parameter_type = parameter_type'
+        ; body = body'
+        }
+    | Kind.Parenthesized { kind; _ } -> remove_parentheses_kind kind
+
+  (** [remove_parentheses_typ typ] is [typ] without parentheses, except for
+      quoted type-level and term-level constants. Printing this type may not
+      result in a syntactically valid LF type. *)
+  and remove_parentheses_typ typ =
+    match typ with
+    | Typ.Constant _ -> typ
+    | Typ.Application { location; applicand; arguments } ->
+      let applicand' = remove_parentheses_typ applicand
+      and arguments' = List.map remove_parentheses_term arguments in
+      Typ.Application
+        { location; applicand = applicand'; arguments = arguments' }
+    | Typ.ForwardArrow { location; domain; range } ->
+      let domain' = remove_parentheses_typ domain
+      and range' = remove_parentheses_typ range in
+      Typ.ForwardArrow { location; domain = domain'; range = range' }
+    | Typ.BackwardArrow { location; domain; range } ->
+      let domain' = remove_parentheses_typ domain
+      and range' = remove_parentheses_typ range in
+      Typ.BackwardArrow { location; domain = domain'; range = range' }
+    | Typ.Pi { location; parameter_identifier; parameter_type; body } ->
+      let parameter_type' = remove_parentheses_typ parameter_type
+      and body' = remove_parentheses_typ body in
+      Typ.Pi
+        { location
+        ; parameter_identifier
+        ; parameter_type = parameter_type'
+        ; body = body'
+        }
+    | Typ.Parenthesized { typ = Typ.Constant _; _ } -> typ
+    | Typ.Parenthesized { typ; _ } -> remove_parentheses_typ typ
+
+  (** [remove_parentheses_term term] is [term] without parentheses, except
+      for quoted type-level and term-level constants. Printing this term may
+      not result in a syntactically valid LF term. *)
+  and remove_parentheses_term term =
+    match term with
+    | Term.Variable _ -> term
+    | Term.Constant _ -> term
+    | Term.Application { location; applicand; arguments } ->
+      let applicand' = remove_parentheses_term applicand
+      and arguments' = List.map remove_parentheses_term arguments in
+      Term.Application
+        { location; applicand = applicand'; arguments = arguments' }
+    | Term.Abstraction
+        { location; parameter_identifier; parameter_type; body } ->
+      let parameter_type' = Option.map remove_parentheses_typ parameter_type
+      and body' = remove_parentheses_term body in
+      Term.Abstraction
+        { location
+        ; parameter_identifier
+        ; parameter_type = parameter_type'
+        ; body = body'
+        }
+    | Term.Wildcard _ -> term
+    | Term.TypeAnnotated { location; term; typ } ->
+      let term' = remove_parentheses_term term
+      and typ' = remove_parentheses_typ typ in
+      Term.TypeAnnotated { location; term = term'; typ = typ' }
+    | Term.Parenthesized { term; _ } -> remove_parentheses_term term
+
+  let add_parentheses_kind kind =
+    let location = location_of_kind kind in
+    Kind.Parenthesized { location; kind }
+
+  let add_parentheses_typ typ =
+    let location = location_of_typ typ in
+    Typ.Parenthesized { location; typ }
+
+  let add_parentheses_term term =
+    let location = location_of_term term in
+    Term.Parenthesized { location; term }
+
+  (** [parenthesize_kind kind] is [kind] with the addition of necessary
+      parentheses for printing.
+
+      If this is done after the application of {!remove_parentheses_kind},
+      then the resultant LF kind can be parsed to the same AST modulo
+      {!remove_parentheses_kind}. *)
+  let rec parenthesize_kind kind =
+    match kind with
+    | Kind.Typ _ -> kind
+    | Kind.Arrow { location; domain; range } ->
+      let domain' =
+        match domain with
+        | Typ.Pi _ | Typ.ForwardArrow _ | Typ.BackwardArrow _ ->
+          add_parentheses_typ (parenthesize_typ domain)
+        | _ -> parenthesize_typ domain
+      and range' = parenthesize_kind range in
+      Kind.Arrow { location; domain = domain'; range = range' }
+    | Kind.Pi { location; parameter_identifier; parameter_type; body } ->
+      let parameter_type' = parenthesize_typ parameter_type
+      and body' = parenthesize_kind body in
+      Kind.Pi
+        { location
+        ; parameter_identifier
+        ; parameter_type = parameter_type'
+        ; body = body'
+        }
+    | Kind.Parenthesized _ -> kind
+
+  (** [parenthesize_typ typ] is [typ] with the addition of necessary
+      parentheses for printing.
+
+      If this is done after the application of {!remove_parentheses_typ},
+      then the resultant LF type can be parsed to the same AST modulo
+      {!remove_parentheses_typ}. *)
+  and parenthesize_typ typ =
+    match typ with
+    | Typ.Constant _ -> typ
+    | Typ.Application
+        { location
+        ; applicand =
+            Typ.Constant { operator = applicand_operator; _ } as applicand
+        ; arguments
+        } ->
+      let applicand_precedence = Operator.precedence applicand_operator in
+      let arguments' =
+        parenthesize_arguments ~applicand_precedence arguments
+      in
+      Typ.Application { location; applicand; arguments = arguments' }
+    | Typ.Application { location; applicand; arguments } ->
+      let applicand' =
+        match applicand with
+        | (Typ.Constant _ | Typ.Parenthesized _) as argument ->
+          parenthesize_typ argument
+        | ( Typ.Application _
+          | Typ.Pi _
+          | Typ.ForwardArrow _
+          | Typ.BackwardArrow _ ) as argument ->
+          add_parentheses_typ (parenthesize_typ argument)
+      in
+      let arguments' = parenthesize_arguments arguments in
+      Typ.Application
+        { location; applicand = applicand'; arguments = arguments' }
+    | Typ.ForwardArrow { location; domain; range } ->
+      let domain' =
+        match domain with
+        | Typ.Pi _ | Typ.ForwardArrow _ | Typ.BackwardArrow _ ->
+          add_parentheses_typ (parenthesize_typ domain)
+        | _ -> parenthesize_typ domain
+      and range' = parenthesize_typ range in
+      Typ.ForwardArrow { location; domain = domain'; range = range' }
+    | Typ.BackwardArrow { location; range; domain } ->
+      let range' =
+        match range with
+        | Typ.Pi _ | Typ.ForwardArrow _ ->
+          add_parentheses_typ (parenthesize_typ range)
+        | _ -> parenthesize_typ range
+      and domain' =
+        match domain with
+        | Typ.BackwardArrow _ ->
+          add_parentheses_typ (parenthesize_typ domain)
+        | _ -> parenthesize_typ domain
+      in
+      Typ.BackwardArrow { location; range = range'; domain = domain' }
+    | Typ.Pi { location; parameter_identifier; parameter_type; body } ->
+      let parameter_type' = parenthesize_typ parameter_type
+      and body' = parenthesize_typ body in
+      Typ.Pi
+        { location
+        ; parameter_identifier
+        ; parameter_type = parameter_type'
+        ; body = body'
+        }
+    | Typ.Parenthesized _ -> typ
+
+  (** [parenthesize_term term] is [term] with the addition of necessary
+      parentheses for printing.
+
+      If this is done after the application of {!remove_parentheses_term},
+      then the resultant LF term can be parsed to the same AST modulo
+      {!remove_parentheses_term}. *)
+  and parenthesize_term term =
+    match term with
+    | Term.Variable _ -> term
+    | Term.Constant _ -> term
+    | Term.Application
+        { location
+        ; applicand =
+            Term.Constant { operator = applicand_operator; _ } as applicand
+        ; arguments
+        } ->
+      let applicand_precedence = Operator.precedence applicand_operator in
+      let arguments' =
+        parenthesize_arguments ~applicand_precedence arguments
+      in
+      Term.Application { location; applicand; arguments = arguments' }
+    | Term.Application { location; applicand; arguments } ->
+      let applicand' =
+        match applicand with
+        | ( Term.Variable _
+          | Term.Constant _
+          | Term.Wildcard _
+          | Term.Parenthesized _ ) as argument -> parenthesize_term argument
+        | (Term.Application _ | Term.Abstraction _ | Term.TypeAnnotated _) as
+          argument -> add_parentheses_term (parenthesize_term argument)
+      in
+      let arguments' = parenthesize_arguments arguments in
+      Term.Application
+        { location; applicand = applicand'; arguments = arguments' }
+    | Term.Abstraction
+        { location; parameter_identifier; parameter_type; body } ->
+      let parameter_type' = Option.map parenthesize_typ parameter_type
+      and body' = parenthesize_term body in
+      Term.Abstraction
+        { location
+        ; parameter_identifier
+        ; parameter_type = parameter_type'
+        ; body = body'
+        }
+    | Term.Wildcard _ -> term
+    | Term.TypeAnnotated { location; term; typ } ->
+      let term' =
+        match term with
+        | Term.Abstraction _ | Term.TypeAnnotated _ ->
+          add_parentheses_term (parenthesize_term term)
+        | _ -> parenthesize_term term
+      and typ' = parenthesize_typ typ in
+      Term.TypeAnnotated { location; term = term'; typ = typ' }
+    | Term.Parenthesized _ -> term
+
+  and parenthesize_arguments ?(applicand_precedence = -1) arguments =
+    List.map
+      (function
+        | Term.Application
+            { applicand = Term.Constant { operator = argument_operator; _ }
+            ; _
+            } as argument ->
+          let argument_precedence = Operator.precedence argument_operator in
+          if argument_precedence > applicand_precedence then
+            add_parentheses_term (parenthesize_term argument)
+          else parenthesize_term argument
+        | ( Term.Variable _
+          | Term.Constant _
+          | Term.Wildcard _
+          | Term.Parenthesized _ ) as argument -> parenthesize_term argument
+        | (Term.Application _ | Term.Abstraction _ | Term.TypeAnnotated _) as
+          argument -> add_parentheses_term (parenthesize_term argument))
+      arguments
+
   let rec pp_kind ppf kind =
     match kind with
     | Kind.Typ _ -> Format.fprintf ppf "type"

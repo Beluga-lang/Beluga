406fed237efc944d2b0c9686e0b6369d92cb8871
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2022-12-22 16:29:36 -0500

    Add `Seq.of_{gen,stream,list}`

--- src/support/seq.ml
+++ src/support/seq.ml
@@ -1,8 +1,29 @@
 include Stdlib.Seq
 
+let rec of_gen g () =
+  match g () with
+  | Option.None -> Nil
+  | Option.Some x -> Cons (x, of_gen g)
+
+let rec of_stream s () =
+  match
+    try Option.some (Stream.next s) with Stream.Failure -> Option.none
+  with
+  | Option.None -> Nil
+  | Option.Some x -> Cons (x, of_stream s)
+
+let of_list l =
+  let rec go l () =
+    match l with
+    | [] -> Nil
+    | x :: xs -> Cons (x, go xs)
+  in
+  go l
+
 let to_list s =
   let rec go s return =
     match s () with
     | Nil -> return []
     | Cons (x, s) -> go s (fun xs -> return (x :: xs))
-  in go s Fun.id
+  in
+  go s Fun.id

7f7f6d17add93e5f05e44600b5cd15deea0f341f
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2022-12-22 16:23:44 -0500

    Add `Apply.lift{2, 3, 4, 5}`

--- src/support/apply.ml
+++ src/support/apply.ml
@@ -27,6 +27,27 @@ module type APPLY = sig
     -> 'a5 t
     -> ('a1 * 'a2 * 'a3 * 'a4 * 'a5) t
 
+  val lift2 : ('a1 -> 'a2 -> 'r) -> 'a1 t -> 'a2 t -> 'r t
+
+  val lift3 : ('a1 -> 'a2 -> 'a3 -> 'r) -> 'a1 t -> 'a2 t -> 'a3 t -> 'r t
+
+  val lift4 :
+       ('a1 -> 'a2 -> 'a3 -> 'a4 -> 'r)
+    -> 'a1 t
+    -> 'a2 t
+    -> 'a3 t
+    -> 'a4 t
+    -> 'r t
+
+  val lift5 :
+       ('a1 -> 'a2 -> 'a3 -> 'a4 -> 'a5 -> 'r)
+    -> 'a1 t
+    -> 'a2 t
+    -> 'a3 t
+    -> 'a4 t
+    -> 'a5 t
+    -> 'r t
+
   val replicate : int -> 'a t -> 'a list t
 end
 
@@ -48,33 +69,64 @@ module Make (M : Monad.MONAD) : APPLY with type 'a t = 'a M.t = struct
   let[@inline] ( &> ) m1 m2 = ap_second m2 m1
 
   let[@inline] seq2 m1 m2 =
-    m1 >>= fun x1 ->
-    m2 >>= fun x2 -> return (x1, x2)
+    let* x1 = m1
+    and* x2 = m2 in
+    return (x1, x2)
 
   let[@inline] seq3 m1 m2 m3 =
-    m1 >>= fun x1 ->
-    m2 >>= fun x2 ->
-    m3 >>= fun x3 -> return (x1, x2, x3)
+    let* x1 = m1
+    and* x2 = m2
+    and* x3 = m3 in
+    return (x1, x2, x3)
 
   let[@inline] seq4 m1 m2 m3 m4 =
-    m1 >>= fun x1 ->
-    m2 >>= fun x2 ->
-    m3 >>= fun x3 ->
-    m4 >>= fun x4 -> return (x1, x2, x3, x4)
+    let* x1 = m1
+    and* x2 = m2
+    and* x3 = m3
+    and* x4 = m4 in
+    return (x1, x2, x3, x4)
 
   let[@inline] seq5 m1 m2 m3 m4 m5 =
-    m1 >>= fun x1 ->
-    m2 >>= fun x2 ->
-    m3 >>= fun x3 ->
-    m4 >>= fun x4 ->
-    m5 >>= fun x5 -> return (x1, x2, x3, x4, x5)
+    let* x1 = m1
+    and* x2 = m2
+    and* x3 = m3
+    and* x4 = m4
+    and* x5 = m5 in
+    return (x1, x2, x3, x4, x5)
+
+  let[@inline] lift2 f m1 m2 =
+    let* x1 = m1
+    and* x2 = m2 in
+    return @@ f x1 x2
+
+  let[@inline] lift3 f m1 m2 m3 =
+    let* x1 = m1
+    and* x2 = m2
+    and* x3 = m3 in
+    return @@ f x1 x2 x3
+
+  let[@inline] lift4 f m1 m2 m3 m4 =
+    let* x1 = m1
+    and* x2 = m2
+    and* x3 = m3
+    and* x4 = m4 in
+    return @@ f x1 x2 x3 x4
+
+  let[@inline] lift5 f m1 m2 m3 m4 m5 =
+    let* x1 = m1
+    and* x2 = m2
+    and* x3 = m3
+    and* x4 = m4
+    and* x5 = m5 in
+    return @@ f x1 x2 x3 x4 x5
 
   let replicate =
     let rec replicate n a =
       if n = 0 then return []
       else
-        a >>= fun x ->
-        replicate (n - 1) a >>= fun xs -> return (List.cons x xs)
+        let* x = a in
+        let* xs = replicate (n - 1) a in
+        return (List.cons x xs)
     in
     fun n ->
       if n < 0 then raise @@ Invalid_argument "replicate" else replicate n

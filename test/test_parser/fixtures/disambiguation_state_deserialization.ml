ce54062f058d4c6d5b573edd368c739752c6b523
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2023-02-01 22:27:35 -0500

    Implement the disambiguation of destructors in expressions

--- test/test_parser/fixtures/disambiguation_state_deserialization.ml
+++ test/test_parser/fixtures/disambiguation_state_deserialization.ml
@@ -7,6 +7,8 @@ exception Unsupported_fixity of string
 
 exception Unsupported_associativity of string
 
+exception Missing_fixity_or_precedence
+
 let fixity_of_json json =
   let open Yojson.Safe.Util in
   match json |> to_string with
@@ -117,6 +119,32 @@ and add_json_entry json =
         | Fixity.Postfix -> Operator.make_postfix ~precedence
       in
       add_computation_term_constructor operator (Identifier.make identifier)
+  | "comp_destructor" ->
+      let identifier = json |> member "identifier" |> to_string in
+      add_computation_term_destructor (Identifier.make identifier)
+  | "program_constant" -> (
+      let identifier = json |> member "identifier" |> to_string in
+      match (member "fixity" json, member "precedence" json) with
+      | `Null, `Null -> add_program_constant (Identifier.make identifier)
+      | `Null, _
+      | _, `Null ->
+          Error.raise Missing_fixity_or_precedence
+      | _ ->
+          let fixity = json |> member "fixity" |> fixity_of_json in
+          let precedence = json |> member "precedence" |> to_int in
+          let operator =
+            match fixity with
+            | Fixity.Prefix ->
+                let arity = json |> member "arity" |> to_int in
+                Operator.make_prefix ~arity ~precedence
+            | Fixity.Infix ->
+                let associativity =
+                  json |> member "associativity" |> associativity_of_json
+                in
+                Operator.make_infix ~associativity ~precedence
+            | Fixity.Postfix -> Operator.make_postfix ~precedence
+          in
+          add_program_constant ~operator (Identifier.make identifier))
   | sort -> Error.raise (Unsupported_sort sort)
 
 let read_disambiguation_state filename =

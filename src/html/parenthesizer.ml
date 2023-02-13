6d92bb8738d2653e16f5c9a6e1e4d88b9498bf96
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2023-02-12 21:19:35 -0500

    Use the state monad for HTML printing
    This makes it easier to manage the state of qualified identifiers for pretty-printing.
    It is arguably more readable to have the `(++)` append combinator than to have a format string followed by the list of printers and arguments to substitute in the format string.
    This implementation also ensures that all boxes are correctly closed, and that parentheses are matched.

--- src/html/parenthesizer.ml
+++ src/html/parenthesizer.ml
@@ -1,27 +1,117 @@
+module MyFormat = Format
 open Support
+open Beluga_syntax
 
-let parenthesize pp ppf = Format.fprintf ppf "@[<hov 2>(%a)@]" pp
+module type PARENTHESIZER = sig
+  include State.STATE
 
-type ('precedence, 'term) parenthesizing_formatter =
-     ('term -> 'precedence)
-  -> parent_precedence:'precedence
-  -> (Format.formatter -> 'term -> Unit.t)
-  -> Format.formatter
-  -> 'term
-  -> Unit.t
+  type precedence
 
-module Make_parenthesizer (Precedence : Ord.ORD) = struct
-  let[@inline] parenthesize_term_of_lesser_precedence precedence
-      ~parent_precedence pp ppf argument =
-    if Precedence.(precedence argument < parent_precedence) then
-      parenthesize pp ppf argument
-    else pp ppf argument
+  val parenthesize_term_of_lesser_precedence :
+       ('a -> precedence)
+    -> parent_precedence:precedence
+    -> ('a -> unit t)
+    -> 'a
+    -> unit t
+
+  val parenthesize_term_of_lesser_than_or_equal_precedence :
+       ('a -> precedence)
+    -> parent_precedence:precedence
+    -> ('a -> unit t)
+    -> 'a
+    -> unit t
+
+  val parenthesize_left_argument_left_associative_operator :
+       ('a -> precedence)
+    -> parent_precedence:precedence
+    -> ('a -> unit t)
+    -> 'a
+    -> unit t
+
+  val parenthesize_right_argument_left_associative_operator :
+       ('a -> precedence)
+    -> parent_precedence:precedence
+    -> ('a -> unit t)
+    -> 'a
+    -> unit t
+
+  val parenthesize_left_argument_right_associative_operator :
+       ('a -> precedence)
+    -> parent_precedence:precedence
+    -> ('a -> unit t)
+    -> 'a
+    -> unit t
+
+  val parenthesize_right_argument_right_associative_operator :
+       ('a -> precedence)
+    -> parent_precedence:precedence
+    -> ('a -> unit t)
+    -> 'a
+    -> unit t
+
+  val parenthesize_argument_non_associative_operator :
+       ('a -> precedence)
+    -> parent_precedence:precedence
+    -> ('a -> unit t)
+    -> 'a
+    -> unit t
+
+  val parenthesize_argument_prefix_operator :
+       ('a -> precedence)
+    -> parent_precedence:precedence
+    -> ('a -> unit t)
+    -> 'a
+    -> unit t
+
+  val parenthesize_argument_postfix_operator :
+       ('a -> precedence)
+    -> parent_precedence:precedence
+    -> ('a -> unit t)
+    -> 'a
+    -> unit t
+
+  val pp_application :
+       guard_operator:('a -> [ `Operator of Operator.t | `Term ])
+    -> guard_operator_application:
+         ('b -> [ `Operator_application of Operator.t | `Term ])
+    -> precedence_of_applicand:('a -> precedence)
+    -> precedence_of_argument:('b -> precedence)
+    -> pp_applicand:('a -> unit t)
+    -> pp_argument:('b -> unit t)
+    -> parent_precedence:precedence
+    -> 'a * 'b List1.t
+    -> unit t
+end
+
+module Make_parenthesizer
+    (Format_state : MyFormat.FORMAT_STATE)
+    (Precedence : Ord.ORD) :
+  PARENTHESIZER
+    with type state = Format_state.state
+     and type precedence = Precedence.t = struct
+  include Format_state
+
+  type precedence = Precedence.t
+
+  let left_parenthesis = pp_char '('
+
+  let right_parenthesis = pp_char ')'
+
+  let parenthesize ppv v = left_parenthesis ++ ppv v ++ right_parenthesis
+
+  let[@inline] parenthesize_term_of_lesser_precedence get_precedence
+      ~parent_precedence pp_argument argument =
+    let precedence = get_precedence argument in
+    if Precedence.(precedence < parent_precedence) then
+      parenthesize pp_argument argument
+    else pp_argument argument
 
   let[@inline] parenthesize_term_of_lesser_than_or_equal_precedence
-      precedence ~parent_precedence pp ppf argument =
-    if Precedence.(precedence argument <= parent_precedence) then
-      parenthesize pp ppf argument
-    else pp ppf argument
+      get_precedence ~parent_precedence pp_argument argument =
+    let precedence = get_precedence argument in
+    if Precedence.(precedence <= parent_precedence) then
+      parenthesize pp_argument argument
+    else pp_argument argument
 
   let parenthesize_left_argument_left_associative_operator =
     parenthesize_term_of_lesser_precedence
@@ -46,33 +136,34 @@ module Make_parenthesizer (Precedence : Ord.ORD) = struct
 
   let rec pp_application ~guard_operator ~guard_operator_application
       ~precedence_of_applicand ~precedence_of_argument ~pp_applicand
-      ~pp_argument ~parent_precedence ppf (applicand, arguments) =
+      ~pp_argument ~parent_precedence (applicand, arguments) =
     match guard_operator applicand with
     | `Term ->
         (* The applicand is not a user-defined operator, so the application
            is in prefix notation. *)
-        Format.fprintf ppf "@[<hov 2>%a@ %a@]"
+        pp_hovbox ~indent:2
           (parenthesize_term_of_lesser_than_or_equal_precedence
-             precedence_of_applicand ~parent_precedence pp_applicand)
-          applicand
-          (List1.pp ~pp_sep:Format.pp_print_space
-             (parenthesize_term_of_lesser_than_or_equal_precedence
-                precedence_of_argument ~parent_precedence pp_argument))
-          arguments
+             precedence_of_applicand ~parent_precedence pp_applicand
+             applicand
+          ++ pp_space
+          ++ pp_list1 ~sep:pp_space
+               (parenthesize_term_of_lesser_than_or_equal_precedence
+                  precedence_of_argument ~parent_precedence pp_argument)
+               arguments)
     | `Operator operator ->
         (* The applicand is a user-defined operator, so pretty-printing must
            handle the operator fixity, associativity and precedence. *)
         pp_operator_application ~guard_operator_application
           ~precedence_of_argument ~pp_applicand ~pp_argument
-          ~parent_precedence applicand operator arguments ppf
+          ~parent_precedence applicand operator arguments
 
   and pp_operator_application ~guard_operator_application
       ~precedence_of_argument ~pp_applicand ~pp_argument ~parent_precedence
-      applicand operator arguments ppf =
+      applicand operator arguments =
     match Operator.fixity operator with
     | Fixity.Prefix ->
         pp_prefix_operator_application ~precedence_of_argument ~pp_applicand
-          ~pp_argument ~parent_precedence applicand arguments ppf
+          ~pp_argument ~parent_precedence applicand arguments
     | Fixity.Infix ->
         assert (
           List1.compare_length_with arguments 2
@@ -83,7 +174,7 @@ module Make_parenthesizer (Precedence : Ord.ORD) = struct
         pp_infix_operator_application ~guard_operator_application
           ~precedence_of_argument ~pp_applicand ~pp_argument
           ~parent_precedence applicand operator ~left_argument
-          ~right_argument ppf
+          ~right_argument
     | Fixity.Postfix ->
         assert (
           List1.compare_length_with arguments 1
@@ -91,165 +182,163 @@ module Make_parenthesizer (Precedence : Ord.ORD) = struct
         let[@warning "-8"] (List1.T (argument, [])) = arguments in
         pp_postfix_operator_application ~guard_operator_application
           ~precedence_of_argument ~pp_applicand ~pp_argument
-          ~parent_precedence applicand argument ppf
+          ~parent_precedence applicand argument
 
   and pp_prefix_operator_application ~precedence_of_argument ~pp_applicand
-      ~pp_argument ~parent_precedence applicand arguments ppf =
-    Format.fprintf ppf "@[<hov 2>%a@ %a@]" pp_applicand applicand
-      (List1.pp ~pp_sep:Format.pp_print_space
-         (parenthesize_argument_prefix_operator precedence_of_argument
-            ~parent_precedence pp_argument))
-      arguments
+      ~pp_argument ~parent_precedence applicand arguments =
+    pp_hovbox ~indent:2
+      (pp_applicand applicand ++ pp_space
+      ++ pp_list1 ~sep:pp_space
+           (parenthesize_argument_prefix_operator precedence_of_argument
+              ~parent_precedence pp_argument)
+           arguments)
 
   and pp_postfix_operator_application ~guard_operator_application
       ~precedence_of_argument ~pp_applicand ~pp_argument ~parent_precedence
-      applicand argument ppf =
-    Format.fprintf ppf "@[<hov 2>%a@ %a@]"
+      applicand argument =
+    pp_hovbox ~indent:2
       (pp_postfix_operator_argument ~guard_operator_application
-         ~precedence_of_argument ~pp_argument ~parent_precedence)
-      argument pp_applicand applicand
+         ~precedence_of_argument ~pp_argument ~parent_precedence argument
+      ++ pp_space ++ pp_applicand applicand)
 
   and pp_infix_operator_application ~guard_operator_application
       ~precedence_of_argument ~pp_applicand ~pp_argument ~parent_precedence
-      applicand operator ~left_argument ~right_argument ppf =
+      applicand operator ~left_argument ~right_argument =
     match Operator.associativity operator with
     | Associativity.Left_associative ->
-        Format.fprintf ppf "@[<hov 2>%a@ %a@ %a@]"
+        pp_hovbox ~indent:2
           (pp_infix_left_associative_operator_left_argument
              ~guard_operator_application ~precedence_of_argument ~pp_argument
-             ~parent_precedence operator)
-          left_argument pp_applicand applicand
-          (pp_infix_left_associative_operator_right_argument
-             ~guard_operator_application ~precedence_of_argument ~pp_argument
-             ~parent_precedence operator)
-          right_argument
+             ~parent_precedence operator left_argument
+          ++ pp_space ++ pp_applicand applicand ++ pp_space
+          ++ pp_infix_left_associative_operator_right_argument
+               ~guard_operator_application ~precedence_of_argument
+               ~pp_argument ~parent_precedence operator right_argument)
     | Associativity.Right_associative ->
-        Format.fprintf ppf "@[<hov 2>%a@ %a@ %a@]"
+        pp_hovbox ~indent:2
           (pp_infix_right_associative_operator_left_argument
              ~guard_operator_application ~precedence_of_argument ~pp_argument
-             ~parent_precedence operator)
-          left_argument pp_applicand applicand
-          (pp_infix_right_associative_operator_right_argument
-             ~guard_operator_application ~precedence_of_argument ~pp_argument
-             ~parent_precedence operator)
-          right_argument
+             ~parent_precedence operator left_argument
+          ++ pp_space ++ pp_applicand applicand ++ pp_space
+          ++ pp_infix_right_associative_operator_right_argument
+               ~guard_operator_application ~precedence_of_argument
+               ~pp_argument ~parent_precedence operator right_argument)
     | Associativity.Non_associative ->
-        Format.fprintf ppf "@[<hov 2>%a@ %a@ %a@]"
+        pp_hovbox ~indent:2
           (pp_infix_non_associative_operator_left_argument
-             ~precedence_of_argument ~pp_argument ~parent_precedence)
-          left_argument pp_applicand applicand
-          (pp_infix_non_associative_operator_right_argument
-             ~precedence_of_argument ~pp_argument ~parent_precedence)
-          right_argument
+             ~precedence_of_argument ~pp_argument ~parent_precedence
+             left_argument
+          ++ pp_space ++ pp_applicand applicand ++ pp_space
+          ++ pp_infix_non_associative_operator_right_argument
+               ~precedence_of_argument ~pp_argument ~parent_precedence
+               right_argument)
 
   and pp_infix_left_associative_operator_left_argument
       ~guard_operator_application ~precedence_of_argument ~pp_argument
-      ~parent_precedence applicand_operator ppf left_argument =
+      ~parent_precedence applicand_operator left_argument =
     match guard_operator_application left_argument with
     | `Term ->
         parenthesize_left_argument_left_associative_operator
-          precedence_of_argument ~parent_precedence pp_argument ppf
-          left_argument
+          precedence_of_argument ~parent_precedence pp_argument left_argument
     | `Operator_application left_argument_operator ->
         if
           Operator.is_right_associative left_argument_operator
           && Operator.precedence left_argument_operator
              = Operator.precedence applicand_operator
-        then (parenthesize pp_argument) ppf left_argument
+        then (parenthesize pp_argument) left_argument
           (* The applications of left and right-associative operators of the
              same precedence must be parenthesized, otherwise the expression
              is ambiguous. *)
         else
           parenthesize_left_argument_left_associative_operator
-            precedence_of_argument ~parent_precedence pp_argument ppf
+            precedence_of_argument ~parent_precedence pp_argument
             left_argument
 
   and pp_infix_left_associative_operator_right_argument
       ~guard_operator_application ~precedence_of_argument ~pp_argument
-      ~parent_precedence applicand_operator ppf right_argument =
+      ~parent_precedence applicand_operator right_argument =
     match guard_operator_application right_argument with
     | `Term ->
         parenthesize_right_argument_left_associative_operator
-          precedence_of_argument ~parent_precedence pp_argument ppf
+          precedence_of_argument ~parent_precedence pp_argument
           right_argument
     | `Operator_application right_argument_operator ->
         if
           Operator.is_right_associative right_argument_operator
           && Operator.precedence right_argument_operator
              = Operator.precedence applicand_operator
-        then (parenthesize pp_argument) ppf right_argument
+        then (parenthesize pp_argument) right_argument
           (* The applications of left and right-associative operators of the
              same precedence must be parenthesized, otherwise the expression
              is ambiguous. *)
         else
           parenthesize_right_argument_left_associative_operator
-            precedence_of_argument ~parent_precedence pp_argument ppf
+            precedence_of_argument ~parent_precedence pp_argument
             right_argument
 
   and pp_infix_right_associative_operator_left_argument
       ~guard_operator_application ~precedence_of_argument ~pp_argument
-      ~parent_precedence applicand_operator ppf left_argument =
+      ~parent_precedence applicand_operator left_argument =
     match guard_operator_application left_argument with
     | `Term ->
         parenthesize_left_argument_right_associative_operator
-          precedence_of_argument ~parent_precedence pp_argument ppf
-          left_argument
+          precedence_of_argument ~parent_precedence pp_argument left_argument
     | `Operator_application left_argument_operator ->
         if
           Operator.is_left_associative left_argument_operator
           && Operator.precedence left_argument_operator
              = Operator.precedence applicand_operator
-        then (parenthesize pp_argument) ppf left_argument
+        then (parenthesize pp_argument) left_argument
           (* The applications of left and right-associative operators of the
              same precedence must be parenthesized, otherwise the expression
              is ambiguous. *)
         else
           parenthesize_left_argument_right_associative_operator
-            precedence_of_argument ~parent_precedence pp_argument ppf
+            precedence_of_argument ~parent_precedence pp_argument
             left_argument
 
   and pp_infix_right_associative_operator_right_argument
       ~guard_operator_application ~precedence_of_argument ~pp_argument
-      ~parent_precedence applicand_operator ppf right_argument =
+      ~parent_precedence applicand_operator right_argument =
     match guard_operator_application right_argument with
     | `Term ->
         parenthesize_right_argument_right_associative_operator
-          precedence_of_argument ~parent_precedence pp_argument ppf
+          precedence_of_argument ~parent_precedence pp_argument
           right_argument
     | `Operator_application right_argument_operator ->
         if
           Operator.is_left_associative right_argument_operator
           && Operator.precedence right_argument_operator
              = Operator.precedence applicand_operator
-        then (parenthesize pp_argument) ppf right_argument
+        then (parenthesize pp_argument) right_argument
           (* The applications of left and right-associative operators of the
              same precedence must be parenthesized, otherwise the expression
              is ambiguous. *)
         else
           parenthesize_right_argument_right_associative_operator
-            precedence_of_argument ~parent_precedence pp_argument ppf
+            precedence_of_argument ~parent_precedence pp_argument
             right_argument
 
   and pp_infix_non_associative_operator_left_argument ~precedence_of_argument
-      ~pp_argument ~parent_precedence ppf left_argument =
+      ~pp_argument ~parent_precedence left_argument =
     parenthesize_argument_non_associative_operator precedence_of_argument
-      ~parent_precedence pp_argument ppf left_argument
+      ~parent_precedence pp_argument left_argument
 
   and pp_infix_non_associative_operator_right_argument
-      ~precedence_of_argument ~pp_argument ~parent_precedence ppf
-      right_argument =
+      ~precedence_of_argument ~pp_argument ~parent_precedence right_argument
+      =
     parenthesize_argument_non_associative_operator precedence_of_argument
-      ~parent_precedence pp_argument ppf right_argument
+      ~parent_precedence pp_argument right_argument
 
   and pp_postfix_operator_argument ~guard_operator_application
-      ~precedence_of_argument ~pp_argument ~parent_precedence ppf argument =
+      ~precedence_of_argument ~pp_argument ~parent_precedence argument =
     match guard_operator_application argument with
     | `Term ->
         parenthesize_argument_postfix_operator precedence_of_argument
-          ~parent_precedence pp_argument ppf argument
+          ~parent_precedence pp_argument argument
     | `Operator_application operator ->
-        if Operator.is_postfix operator then pp_argument ppf argument
+        if Operator.is_postfix operator then pp_argument argument
         else
           parenthesize_argument_postfix_operator precedence_of_argument
-            ~parent_precedence pp_argument ppf argument
+            ~parent_precedence pp_argument argument
 end

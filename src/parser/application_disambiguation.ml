4552f3fccd8d81004bed608cfbb954f643c1a557
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2023-02-24 18:53:50 -0500

    Implement fallback application disambiguation
    Application disambiguation using the shunting yard algorithm is the fastest, but produces confusing errors.
    
      Time (mean ± σ):      1.459 s ±  0.032 s    [User: 17.774 s, System: 1.271 s]
      Range (min … max):    1.416 s …  1.513 s    10 runs
    
    Application disambiguation using recursive descent parsing is the slowest, but produces good errors.
    
      Time (mean ± σ):      1.498 s ±  0.032 s    [User: 18.263 s, System: 1.374 s]
      Range (min … max):    1.433 s …  1.547 s    10 runs
    
    This new application disambiguation first disambiguates using the shunting yard algorithm, then using the parsing algorithm in case of failure.
    
      Time (mean ± σ):      1.471 s ±  0.035 s    [User: 18.019 s, System: 1.273 s]
      Range (min … max):    1.421 s …  1.527 s    10 runs

--- src/parser/application_disambiguation.ml
+++ src/parser/application_disambiguation.ml
@@ -37,5 +37,84 @@ end
 module Make_application_parser = Application_parser.Make_application_parser
 module Make_application_rewriter = Shunting_yard.Make_application_rewriter
 
-(* Choose one of [Make_application_parser] or [Make_application_rewriter]. *)
-module Make_application_disambiguation = Make_application_parser
+module Make_fallback_application_disambiguation
+    (Disambiguation : APPLICATION_DISAMBIGUATION)
+    (Error_disambiguation : APPLICATION_DISAMBIGUATION
+                              with type expression =
+                                Disambiguation.expression) :
+  APPLICATION_DISAMBIGUATION
+    with type expression = Disambiguation.expression
+     and type target = Disambiguation.target = struct
+  exception Inconsistent_application_disambiguation
+
+  type expression = Disambiguation.expression
+
+  type source =
+    | Expression of expression
+    | Operator of
+        { expression : expression
+        ; operator : Operator.t
+        ; identifier : Qualified_identifier.t
+        }
+
+  let[@inline] make_expression expression = Expression expression
+
+  let[@inline] make_operator expression operator identifier =
+    Operator { expression; operator; identifier }
+
+  type target = Disambiguation.target = private
+    | Atom of
+        { expression : expression
+        ; location : Location.t
+        }
+    | Application of
+        { applicand : expression
+        ; arguments : target List1.t
+        ; location : Location.t
+        }
+
+  let prepare_disambiguation_expression = function
+    | Expression expression -> Disambiguation.make_expression expression
+    | Operator { expression; operator; identifier } ->
+        Disambiguation.make_operator expression operator identifier
+
+  let prepare_error_expression = function
+    | Expression expression ->
+        Error_disambiguation.make_expression expression
+    | Operator { expression; operator; identifier } ->
+        Error_disambiguation.make_operator expression operator identifier
+
+  let disambiguate_application expressions =
+    let disambiguation_expressions =
+      List2.map prepare_disambiguation_expression expressions
+    in
+    try
+      Disambiguation.disambiguate_application disambiguation_expressions
+    with
+    | _ ->
+        (* Disambiguation using [Disambiguation] failed. Disambiguation with
+           [Error_disambiguation] must fail, and provide a better error to
+           report. *)
+        let error_expressions =
+          List2.map prepare_error_expression expressions
+        in
+        ignore
+          (Error_disambiguation.disambiguate_application error_expressions);
+        Error.raise Inconsistent_application_disambiguation
+end
+
+module Make_rewrite_parse_application_disambiguation
+    (Expression : EXPRESSION with type location = Location.t) :
+  APPLICATION_DISAMBIGUATION with type expression = Expression.t = struct
+  module Disambiguation = Make_application_parser (Expression)
+  module Error_disambiguation = Make_application_rewriter (Expression)
+  include
+    Make_fallback_application_disambiguation
+      (Disambiguation)
+      (Error_disambiguation)
+end
+
+(* Choose one of [Make_application_parser], [Make_application_rewriter] or
+   [Make_rewrite_parse_application_disambiguation]. *)
+module Make_application_disambiguation =
+  Make_rewrite_parse_application_disambiguation

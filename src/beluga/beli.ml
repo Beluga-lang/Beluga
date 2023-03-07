4779ee5b8d6c962284fcbbdee2d015531863d132
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2023-03-07 12:01:20 -0500

    Fix control flow bug in `run`
    The `try-with` expression would catch all `Failure` exceptions occurring in signautre reconstruction.

--- src/beluga/beli.ml
+++ src/beluga/beli.ml
@@ -1,5 +1,4 @@
 open Support
-open Format
 open Beluga
 
 module LF = Syntax.Int.LF
@@ -31,14 +30,14 @@ let rec process_options = function
       arg :: rest
 
 let init_repl ppf =
-  fprintf ppf "        Beluga (interactive) version %s@.@.%s"
+  Format.fprintf ppf "        Beluga (interactive) version %s@.@.%s"
     (Version.get ())
     Interactive.whale
 
 let rec loop ppf =
   begin
     try
-      if Bool.not !Options.emacs then fprintf ppf "# @?";
+      if Bool.not !Options.emacs then Format.fprintf ppf "# @?";
       let input = read_line () in
       match Command.is_command input with
       | `Cmd cmd ->
@@ -50,16 +49,16 @@ let rec loop ppf =
            |> Interactive.elaborate_exp' LF.Empty LF.Empty
            |> Pair.map_right Whnf.cnormCTyp
          in
-         fprintf ppf "%a : %a"
+         Format.fprintf ppf "%a : %a"
            (P.fmt_ppr_cmp_exp LF.Empty LF.Empty P.l0) exp
            (P.fmt_ppr_cmp_typ LF.Empty P.l0) tau
       | `Input _ -> ()
     with
       | End_of_file -> exit 0
       | Sys.Break ->
-          if !Options.interrupt_count = 2 then begin fprintf ppf "@\n@."; exit 0 end else begin
+          if !Options.interrupt_count = 2 then begin Format.fprintf ppf "@\n@."; exit 0 end else begin
           incr Options.interrupt_count;
-          fprintf ppf "Interrupted (press %d more times to quit).@." (3 - !Options.interrupt_count) end
+          Format.fprintf ppf "Interrupted (press %d more times to quit).@." (3 - !Options.interrupt_count) end
       | err ->
         output_string stderr (Printexc.to_string err);
         flush stderr
@@ -71,17 +70,15 @@ let run args =
   let files = process_options args in
   Debug.init (Some "debug.out");
 
-  if List.length files = 1 then
-    try
-      let arg = List.hd files in
+  match files with
+  | [] -> Format.fprintf ppf "Please provide the file name.\n"
+  | _ :: _ :: _ -> Format.fprintf ppf "Please supply only 1 file.\n"
+  | [ arg ] ->
       let _location = Beluga_syntax.Location.initial arg in
       let[@warning "-26"] sgn = Obj.magic () (* TODO: Parse only a signature from the file [arg] *) in
       let sgn' = Obj.magic () (* TODO: Reconstruct [sgn] and raise on leftover variables *) in
       Chatter.print 1 (fun ppf -> Format.fprintf ppf "%a" P.fmt_ppr_sgn sgn');
-      fprintf ppf "The file has been successfully loaded.\n"
-    with
-    |Failure _ -> fprintf ppf "Please provide the file name\n" ;
-  else begin if List.length files > 1 then fprintf ppf "Please supply only 1 file" end;
+      Format.fprintf ppf "The file has been successfully loaded.\n";
 
   if Bool.not !Options.emacs then
     begin

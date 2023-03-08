37056520fbbccb5e270c18bbd0af6f53659bc920
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2023-03-08 15:03:45 -0500

    Ajust the control flow

--- src/beluga/version.ml
+++ src/beluga/version.ml
@@ -1,5 +1,7 @@
+open Support
+
 (** Gets the version of Beluga from the dune-build.info library. *)
 let get () =
   match Build_info.V1.version () with
-  | None -> "n/a"
-  | Some v -> Build_info.V1.Version.to_string v
+  | Option.None -> "n/a"
+  | Option.Some v -> Build_info.V1.Version.to_string v

03ecd6e838d872049a0d75165417fcfa1333453e
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2023-03-09 17:13:30 -0500

    Fix formatting

--- src/support/format.mli
+++ src/support/format.mli
@@ -1,7 +1,6 @@
 include module type of Stdlib.Format
 
-type fmt =
-  { fmt : 'a. ('a, formatter, unit) format -> 'a }
+type fmt = { fmt : 'a. ('a, formatter, unit) format -> 'a }
 
 val stringify : (formatter -> 'a -> unit) -> 'a -> string
 

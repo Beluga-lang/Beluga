f4c950b42b94cd239e971792000fbca098347b6b
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2023-03-09 16:19:18 -0500

    Remove unused `Options.Monitor` module

--- src/core/options.ml
+++ src/core/options.ml
@@ -32,25 +32,3 @@ module HTML = struct
   let filename = ref (Some "out.html")
   let css : css_file ref = ref `normal
 end
-
-module Monitor = struct
-  (* TODO this should be refactored as a single variable holding an
-     option containing the path to dump to. None signifies stdout.
-     -je
-   *)
-
-  (** on: is true if we call the interpreter with argument: +t
-      The result is to print the timings to stdout when signature
-      reconstruction ends.
-   *)
-  let on = ref false
-
-  (** onf: is true if we call the interpreter with argument: +tfile
-      The result is to dump the timing to a file when signature
-      reconstruction ends.
-   *)
-  let onf = ref false
-
-  (** Decides whether either timing mode (stdout or file) is active *)
-  let is_active () = !on || !onf
-end

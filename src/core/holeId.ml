5ea5fe6c3793459a1d9198cd6dd7085879a456e9
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2023-03-15 20:07:32 -0400

    Format and refactor module

--- src/core/holeId.ml
+++ src/core/holeId.ml
@@ -1,9 +1,12 @@
+open Support
+
 type t = int
 
 let of_int x = x
 
 module Make () = struct
   let counter = ref 0
+
   let next () =
     let x = !counter in
     incr counter;
@@ -16,38 +19,30 @@ type name =
   | Anonymous
   | Named of string
 
-let is_anonymous =
-  function
+let is_anonymous = function
   | Anonymous -> true
-  | _ -> false
+  | Named _ -> false
 
-let name_of_option : string option -> name =
-  function
-  | Some name -> Named name
-  | None -> Anonymous
+let name_of_option : string option -> name = function
+  | Option.Some name -> Named name
+  | Option.None -> Anonymous
 
-let option_of_name : name -> string option =
-  function
-  | Anonymous -> None
-  | Named s -> Some s
+let option_of_name : name -> string option = function
+  | Anonymous -> Option.none
+  | Named s -> Option.some s
 
-let string_of_name : name -> string =
-  function
+let string_of_name : name -> string = function
   | Anonymous -> ""
   | Named s -> s
 
-let string_of_name_or_id : name * t -> string =
-  function
+let string_of_name_or_id : name * t -> string = function
   | Anonymous, i -> string_of_hole_id i
   | Named s, _ -> s
 
-let fmt_ppr_name ppf : name -> unit =
-  let open Format in
-  function
-  | Anonymous -> fprintf ppf "<anonymous>"
-  | Named s -> fprintf ppf "?%s" s
+let fmt_ppr_name ppf : name -> unit = function
+  | Anonymous -> Format.fprintf ppf "<anonymous>"
+  | Named s -> Format.fprintf ppf "?%s" s
 
-let fmt_ppr_id ppf (id : t) =
-  Format.fprintf ppf "%d" id
+let fmt_ppr_id = Format.pp_print_int
 
 let compare = compare
--- src/core/holeId.mli
+++ src/core/holeId.mli
@@ -5,14 +5,14 @@ type name =
 type t
 
 val of_int : int -> t
+
 val string_of_hole_id : t -> string
 
 (** Construct a counter for hole id numbers.
-    The reason this is wrapped in a dummy functor is that functor
-    instantiation allows the get_next_key function to operate on a
-    private integer reference, that can be made inaccessible outside
-    of holes.ml
- *)
+
+    The reason this is wrapped in a generative functor is that functor
+    instantiation allows the [next] function to operate on a private integer
+    reference, that can be made inaccessible outside of this module. *)
 module Make () : sig
   (** Generates the next hole ID. *)
   val next : unit -> t
@@ -24,18 +24,19 @@ val name_of_option : string option -> name
 (** Converts a name to an option type. *)
 val option_of_name : name -> string option
 
-(** Stringifies a hole name.
- * If the hole is anonymous, then its name is the empty string. *)
+(** Stringifies a hole name. * If the hole is anonymous, then its name is the
+    empty string. *)
 val string_of_name : name -> string
 
-(** Stringifies a hole name.
- * If the hole is anonymous, then use its hole id. *)
+(** Stringifies a hole name. * If the hole is anonymous, then use its hole
+    id. *)
 val string_of_name_or_id : name * t -> string
 
 (** Decides whether a hole name is anonymous. *)
 val is_anonymous : name -> bool
 
 val fmt_ppr_name : Format.formatter -> name -> unit
+
 val fmt_ppr_id : Format.formatter -> t -> unit
 
 val compare : t -> t -> int

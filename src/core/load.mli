11d17379a8086882decba75c9eec9a1d59147603
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2023-04-01 11:07:36 -0400

    Format module

--- src/core/load.mli
+++ src/core/load.mli
@@ -1,8 +1,6 @@
 (** Loads one file *on top* of what has already been loaded. *)
 val load_one : Format.formatter -> string -> unit
 
-(** Clears all internal state and loads the given path to a cfg or bel
-    file.
-    The list of resolved paths (paths to bel files) is returned.
- *)
+(** Clears all internal state and loads the given path to a [.cfg] or [.bel]
+    file. The list of resolved paths (paths to [.bel] files) is returned. *)
 val load : Format.formatter -> string -> string list

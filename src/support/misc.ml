150f50b67ac3e628c9674e3914ea749c6ebb9883
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2023-04-27 15:56:41 -0400

    Remove `Misc.enumerate`
    This was just a less performant implementation of `List.index`.

--- src/support/misc.ml
+++ src/support/misc.ml
@@ -15,25 +15,6 @@ exception NotImplemented of string
 
 let not_implemented (msg : string) : 'a = raise (NotImplemented msg)
 
-(** Enumerates a list using a state transformer to generate indices. The
-    initial seed [s] contains the initial state and the function [f]
-    transforms this state to compute a new state and an index. These indices
-    are sequentially zipped with the given list to produce an indexed list,
-    and the final state is returned. *)
-let rec enumerate_with_state (s : 's) (f : 's -> 's * 'i) (l : 'a list) :
-    's * ('i * 'a) list =
-  match l with
-  | [] -> (s, [])
-  | x :: xs ->
-      let s', i = f s in
-      let y = (i, x) in
-      let s_final, ys = enumerate_with_state s' f xs in
-      (s_final, y :: ys)
-
-(** Enumerates a list by pairing each element with its index. *)
-let enumerate (l : 'a list) : (int * 'a) list =
-  enumerate_with_state 0 (fun s -> (s + 1, s)) l |> Pair.snd
-
 (** Forms the tuple of its two inputs. *)
 let tuple (x : 'a) (y : 'b) : 'a * 'b = (x, y)
 

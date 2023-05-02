725e4baf8c3a18a419d2decc7555b19e6146efca
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2023-05-02 19:37:23 -0400

    Implement prompt parsing for next theorem names

--- src/harpoon/iO.mli
+++ src/harpoon/iO.mli
@@ -2,7 +2,7 @@
 
 open Beluga_syntax
 
-type exn += private Io_error of exn
+exception Io_error of exn
 
 (** Type of IO capabilities. Includes:
 
@@ -28,14 +28,33 @@ val printf : t -> ('a, Format.formatter, unit) format -> 'a
 (** Default value used for the filename when displaying parse errors. *)
 val default_prompt_source : string
 
-(** Prompts the user for input by displaying the given message. The input
-    line is output.
+(** [read_line ?source io ~msg ~history_file] prompts the user for input by
+    displaying the given message. The input line is output. The returned
+    location is an empty location having [source] as filename and the number
+    of lines input so far as the line number.
 
-    The default value of the optional `source` parameter is
-    {!default_source_prompt}. *)
+    The default value of [?source] is {!val:default_source_prompt}.
+
+    An exception is raised if there are no more lines in the input. *)
 val read_line :
      ?source:string
   -> t
   -> msg:string
   -> history_file:string option
   -> Location.t * string
+
+(** [prompt_input ?source io ~msg ~history_file f] repeatedly prompts the
+    user for an input to pass to [f] until a call to [f] does not throw. That
+    is, it calls [read_line ?source io ~msg ~history_file], then passes the
+    result to [f]. If [f] raises an exception, then it is caught, printed,
+    and [prompt_input] is called once more.
+
+    Make sure that [f] can fail gracefully (by cleaning up in case of a
+    raised exception) because any exception from [f] will be suppressed. *)
+val prompt_input :
+     ?source:string
+  -> t
+  -> msg:string
+  -> history_file:string option
+  -> (Location.t -> string -> 'a option)
+  -> 'a option

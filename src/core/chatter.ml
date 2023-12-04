2e24420ec49784d1115e32c3c103c102e66c447f
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2023-12-04 10:02:56 -0500

    Flush the output buffer on every call to `Chatter.print`
    This ensures that logged messages during the signature loading procedure are printed when they are emitted.
    Before this change, the messages would only appear after signature reconstruction was completed.

--- src/core/chatter.ml
+++ src/core/chatter.ml
@@ -4,5 +4,5 @@ let level : int ref = ref 1
 
 let print lvl f =
   if lvl <= !level
-  then (f Format.std_formatter)
+  then (f Format.std_formatter; Format.pp_print_flush Format.std_formatter ())
   else ()
